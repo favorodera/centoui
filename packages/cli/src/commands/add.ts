7da53686a0aff9165d9d86745cfe6350a56ea4c8
Favour Emeka <favorodera@gmail.com>
2026-04-29 22:21:57 +0100

    refactor: Uncomment and enable add command functionality

--- packages/cli/src/commands/add.ts
+++ packages/cli/src/commands/add.ts
@@ -1,27 +1,27 @@
-// import { defineCommand } from 'citty'
-// import { loadUserConfig } from '../utils/config-utils'
+import { defineCommand } from 'citty'
+import { loadUserConfig } from '../utils/config-utils'
 
-// /**
-//  * Command: add
-//  *
-//  * Installs one or more components (and their transitive dependencies) from
-//  * the registry into the user's project.
-//  */
-// export function add() {
-//   return defineCommand({
-//     meta: {
-//       name: 'add',
-//       description: 'Add one or more components to your project',
-//     },
+/**
+ * Command: add
+ *
+ * Installs one or more components (and their transitive dependencies) from
+ * the registry into the user's project.
+ */
+export function add() {
+  return defineCommand({
+    meta: {
+      name: 'add',
+      description: 'Add one or more components to your project',
+    },
 
-//     args: {
-//       // ARGS is kept empty to accommodate multiple component names as arguments
-//     },
+    args: {
+      // ARGS is kept empty to accommodate multiple component names as arguments
+    },
 
-//     // async run({ args }) {
-//     //   const cwd = process.cwd()
-//     //   const userConfig = await loadUserConfig(cwd)
-//     //   const componentsToAdd = args._
-//     // },
-//   })
-// }
+    async run({ args }) {
+      const cwd = process.cwd()
+      const userConfig = await loadUserConfig(cwd)
+      const componentsToAdd = args._
+    },
+  })
+}
