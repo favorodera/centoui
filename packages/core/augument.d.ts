469537b89cfbf5465aab9f2485113fa31d9eced9
Favour Emeka <favorodera@gmail.com>
2026-05-03 16:42:23 +0100

    chore: remove obsolete vitest-axe type augmentation

--- packages/core/augument.d.ts
+++
@@ -1,9 +0,0 @@
-import 'vitest'
-import { AxeMatchers } from 'vitest-axe/matchers'
-
-declare module 'vitest' {
-  export interface Assertion extends AxeMatchers {}
-  export interface AsymmetricMatchersContaining extends AxeMatchers {}
-}
-
-export {}
