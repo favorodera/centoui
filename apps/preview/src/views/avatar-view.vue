1dcdbcbbc25de039bdbb751278fbd753e0f6b3cc
Favour Emeka <favorodera@gmail.com>
2026-05-04 10:02:46 +0100

    refactor: Update Avatar component props and fallback logic
    The Avatar component's props have been refactored to leverage the `avatarVariants` utility for dynamic size options. The fallback logic has also been simplified, removing direct props for `iconFallback` and `textFallback` and instead relying on the `AvatarFallback` slot for more flexible rendering. The example usage in `avatar-view.vue` has been updated to reflect these changes.

--- apps/preview/src/views/avatar-view.vue
+++ apps/preview/src/views/avatar-view.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
-import { AvatarRoot, AvatarImage, AvatarFallback, getInitials } from '../../../../packages/core/src/components/avatar'
+import { AvatarRoot, AvatarImage, AvatarFallback, avatarVariants } from '../../../../packages/core/src/components/avatar'
 import { Icon } from '@iconify/vue'
 import { usePreview } from '../utils/use-preview'
 import type { PropsSchema } from '../components/props-panel.vue'
@@ -9,32 +9,16 @@ import ViewContainer from '../components/view-container.vue'
 const { values, setPreviewState } = usePreview()
 
 const avatarPropsSchema: PropsSchema = {
-  src: {
-    type: 'string',
-    label: 'Image URL',
-    default: 'https://github.com/favorodera.png',
-  },
   size: {
     type: 'select',
     label: 'Size',
-    options: ['3xs', '2xs', 'xs', 'sm', 'md', 'lg', 'xl', '2xl', '3xl'],
+    options: Object.keys(avatarVariants.variants.size),
     default: 'md',
   },
   disabled: {
     type: 'boolean',
     label: 'Disabled',
     default: false,
-  },
-  iconFallback: {
-    type: 'string',
-    label: 'Icon Fallback',
-    hint: 'Enter an Iconify icon name (e.g., lucide:plus)',
-    default: '',
-  },
-  textFallback: {
-    type: 'string',
-    label: 'Text Fallback',
-    default: 'Favour Emeka',
   },
 }
 
@@ -45,20 +29,41 @@ onMounted(() => {
 
 <template>
   <ViewContainer>
-      
     <AvatarRoot
       :size="values.size"
       :disabled="values.disabled"
     >
-      <AvatarImage :src="values.src" />
-      <AvatarFallback>
-        <Icon
-          v-if="values.iconFallback"
-          :icon="values.iconFallback"
-        />
-        <span v-else-if="values.textFallback">{{ getInitials(values.textFallback) }}</span>
-      </AvatarFallback>
+      <AvatarImage
+        src="https://github.com/favorodera.png"
+        alt="Image"
+      />
     </AvatarRoot>
 
+    <AvatarRoot
+      :size="values.size"
+      :disabled="values.disabled"
+    >
+      <AvatarImage
+        src=""
+        alt="Image"
+      />
+      <AvatarFallback>FE</AvatarFallback>
+    </AvatarRoot>
+
+    <AvatarRoot
+      :size="values.size"
+      :disabled="values.disabled"
+    >
+      <AvatarImage
+        src=""
+        alt="Image"
+      />
+      <AvatarFallback>
+        <Icon
+          icon="lucide:user"
+        />
+      </AvatarFallback>
+    </AvatarRoot>
+      
   </ViewContainer>
 </template>
