bbc4cd020136f03625a932aace9c54399007270e
Favour Emeka <favorodera@gmail.com>
2026-05-04 17:39:23 +0100

    feat: add Separator component preview

---
+++ apps/preview/src/views/separator-view.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { Separator, separatorVariants } from '../../../../packages/core/src/components/separator'
+import { Icon } from '@iconify/vue'
+import { usePreview } from '../utils/use-preview'
+import type { PropsSchema } from '../components/props-panel.vue'
+import ViewContainer from '../components/view-container.vue'
+
+const { values, setPreviewState } = usePreview()
+
+const separatorPropsSchema: PropsSchema = {
+  size: {
+    type: 'select',
+    label: 'Size',
+    options: Object.keys(separatorVariants.variants.size),
+    default: 'xs',
+  },
+  color: {
+    type: 'select',
+    label: 'Color',
+    options: Object.keys(separatorVariants.variants.color),
+    default: 'neutral',
+  },
+  variant: {
+    type: 'select',
+    label: 'Variant',
+    options: Object.keys(separatorVariants.variants.variant),
+    default: 'solid',
+  },
+  orientation: {
+    type: 'select',
+    label: 'Orientation',
+    options: Object.keys(separatorVariants.variants.orientation),
+    default: 'horizontal',
+  },
+}
+
+onMounted(() => {
+  setPreviewState('Separator', separatorPropsSchema)
+})
+</script>
+
+<template>
+  <ViewContainer>
+    <Separator
+      :size="values.size"
+      :color="values.color"
+      :variant="values.variant"
+      :orientation="values.orientation"
+    />
+
+    <Separator
+      :size="values.size"
+      :color="values.color"
+      :variant="values.variant"
+      :orientation="values.orientation"
+    >
+      <Icon icon="lucide:user" />
+    </Separator>
+
+    <Separator
+      :size="values.size"
+      :color="values.color"
+      :variant="values.variant"
+      :orientation="values.orientation"
+    >
+      Hello
+    </Separator>
+  </ViewContainer>
+</template>
