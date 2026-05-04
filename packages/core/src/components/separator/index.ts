bbfcb45164746bd625856ccb4d540ca0fea7a9e1
Favour Emeka <favorodera@gmail.com>
2026-05-04 10:32:46 +0100

    feat: add Separator component

---
+++ packages/core/src/components/separator/index.ts
@@ -0,0 +1,29 @@
+import {
+  type SeparatorProps as RekaSeparatorProps,
+} from 'reka-ui'
+import { tv, type ClassValue } from 'tailwind-variants'
+
+export const separatorVariants = tv({
+  slots: {
+    root: [
+      
+    ],
+  },
+  variants: {},
+  compoundVariants: [],
+  defaultVariants: {},
+})
+
+export { default as Separator } from './separator.vue'
+
+export type SeparatorVariants = ReturnType<typeof separatorVariants>
+
+export type SeparatorProps = RekaSeparatorProps & {
+  /** Custom class to apply to the root element. */
+  class?: ClassValue
+}
+
+export type SeparatorSlots = {
+  /** Separator content - text, icons, etc. */
+  default: []
+}
