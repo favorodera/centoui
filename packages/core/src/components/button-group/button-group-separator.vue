907eed92d4862ce1e118b54461071c6bad42df8b
Favour Emeka <favorodera@gmail.com>
2026-05-05 11:11:00 +0100

    feat: add button group separator component

---
+++ packages/core/src/components/button-group/button-group-separator.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
+import { computed, inject, useAttrs } from 'vue'
+import { buttonGroupContextKey, buttonGroupVariants, type ButtonGroupSeparatorProps } from '.'
+import { Separator, type SeparatorProps } from '../separator'
+
+defineOptions({
+  inheritAttrs: false,
+})
+
+const attributes = useAttrs()
+
+const props = defineProps<ButtonGroupSeparatorProps>()
+  
+const delegatedProps = reactiveOmit(props, 'class')
+
+const styles = computed(() => {
+  const { separator } = buttonGroupVariants()
+  return separator({ class: props.class })
+})
+
+const buttonGroupContext = inject(buttonGroupContextKey, { orientation: 'vertical' })
+
+const orientation = computed<SeparatorProps['orientation']>(() => {
+  return buttonGroupContext.orientation === 'vertical' ? 'horizontal' : 'vertical'
+})
+</script>
+
+<template>
+  <Separator
+    data-centoui-slot="button-group-separator"
+    v-bind="{...delegatedProps, ...attributes}"
+    :orientation="orientation"
+    :class="styles"
+  />
+</template>
