7034f2975cb491c886ce29a96903c3920b22618b
Favour Emeka <favorodera@gmail.com>
2026-05-05 11:08:16 +0100

    style: Add flex-wrap to view-container for responsiveness

--- apps/preview/src/components/view-container.vue
+++ apps/preview/src/components/view-container.vue
@@ -1,7 +1,8 @@
 <template>
   <div
     class="
-      relative flex h-full items-center justify-center gap-12 bg-background p-8
+      relative flex h-full flex-wrap items-center justify-center gap-12
+      bg-background p-8
     "
   >
     <slot />
