1a3859a9a00465e2531b8d7f5d1dad5ea325c1be
Favour Emeka <favorodera@gmail.com>
2026-05-05 11:12:53 +0100

    feat: add ButtonGroup component

---
+++ packages/core/src/components/button-group/button-group.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { Primitive, useForwardProps } from 'reka-ui'
+import { buttonGroupContextKey, buttonGroupVariants, type ButtonGroupProps, type ButtonGroupSlots } from '.'
+import { reactiveOmit } from '@vueuse/core'
+import { computed, provide, reactive, toRef } from 'vue'
+
+defineSlots<ButtonGroupSlots>()
+
+const props = withDefaults(defineProps<ButtonGroupProps>(), {
+  as: 'div',
+  orientation: 'horizontal',
+})
+
+const delegatedProps = reactiveOmit(props, 'class', 'orientation')
+const forwardedProps = useForwardProps(delegatedProps)
+
+const styles = computed(() => {
+  const { root } = buttonGroupVariants({
+    orientation: props.orientation,
+  })
+  return root({ class: props.class })
+})
+
+provide(
+  buttonGroupContextKey,
+  reactive({
+    orientation: toRef(props, 'orientation'),
+  }),
+)
+</script>
+
+<template>
+  <Primitive
+    role="group"
+    data-centoui-slot="button-group-root"
+    :data-centoui-orientation="props.orientation"
+    v-bind="forwardedProps"
+    :class="styles"
+  >
+    <slot />
+  </Primitive>
+</template>
