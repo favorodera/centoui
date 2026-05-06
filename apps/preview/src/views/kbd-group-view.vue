426497b253081ecb8c0e2e962267d2fd3e854ebb
Favour Emeka <favorodera@gmail.com>
2026-05-06 12:42:59 +0100

    feat: add KbdGroup preview view

---
+++ apps/preview/src/views/kbd-group-view.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { Kbd } from '../../../../packages/core/src/components/kbd'
+import { KbdGroup } from '../../../../packages/core/src/components/kbd-group'
+import { usePreview } from '../utils/use-preview'
+import type { PropsSchema } from '../components/props-panel.vue'
+import ViewContainer from '../components/view-container.vue'
+
+const { setPreviewState } = usePreview()
+
+const kbdGroupPropsSchema: PropsSchema = {}
+
+onMounted(() => {
+  setPreviewState('Kbd Group', kbdGroupPropsSchema)
+})
+</script>
+
+<template>
+  <ViewContainer>
+    <KbdGroup>
+      <Kbd>
+        ⌘
+      </Kbd>
+
+      <Kbd>
+        ⇧
+      </Kbd>
+    </KbdGroup>
+
+    <KbdGroup>
+      <Kbd>
+        ⌥
+      </Kbd>
+
+      <Kbd>
+        ⌃
+      </Kbd>
+    </KbdGroup>
+  </ViewContainer>
+</template>
