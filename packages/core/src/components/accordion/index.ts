562834a46c091058c2654471ac16d4439707e7f1
Favour Emeka <favorodera@gmail.com>
2026-05-06 20:33:06 +0100

    feat: export AccordionItem and add default borders

--- packages/core/src/components/accordion/index.ts
+++ packages/core/src/components/accordion/index.ts
@@ -13,7 +13,7 @@ import type {
 export const accordionVariants = tv({
   slots: {
     root: 'w-full',
-    item: '',
+    item: 'border-b last:border-b-0 border-b-muted',
     header: '',
     trigger: '',
     content: '',
@@ -25,7 +25,7 @@ export const accordionVariants = tv({
 
 // Component exports
 export { default as AccordionRoot } from './accordion-root.vue'
-// export { default as AccordionItem } from './accordion-item.vue'
+export { default as AccordionItem } from './accordion-item.vue'
 // export { default as AccordionHeader } from './accordion-header.vue'
 // export { default as AccordionTrigger } from './accordion-trigger.vue'
 // export { default as AccordionContent } from './accordion-content.vue'
