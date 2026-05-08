e8cbb59ab0e1a0a9cbb5b05097d609c5e85d7462
Favour Emeka <favorodera@gmail.com>
2026-05-08 11:35:33 +0100

    feat: add TooltipPortal component

---
+++ packages/core/src/components/tooltip/tooltip-portal.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { TooltipPortal, useForwardProps } from 'reka-ui'
+import { type TooltipPortalProps, type TooltipPortalSlots } from './index'
+
+defineOptions({
+  inheritAttrs: false,
+})
+
+defineSlots<TooltipPortalSlots>()
+
+const props = defineProps<TooltipPortalProps>()
+
+// Forward props.
+const forwardedProps = useForwardProps(props)
+</script>
+
+<template>
+  <TooltipPortal
+    v-bind="forwardedProps"
+  >
+    <slot />
+  </TooltipPortal>
+</template>
