46f373c5f058fefc3b85c9b43d8e0ec88d60f5ac
Favour Emeka <favorodera@gmail.com>
2026-05-08 11:36:55 +0100

    feat: add TooltipRoot component

---
+++ packages/core/src/components/tooltip/tooltip-root.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { TooltipRoot, useForwardPropsEmits } from 'reka-ui'
+import { type TooltipRootEmits, type TooltipRootProps, type TooltipRootSlots } from './index'
+
+defineSlots<TooltipRootSlots>()
+  
+const emits = defineEmits<TooltipRootEmits>()
+
+const props = defineProps<TooltipRootProps>()
+
+// Forward props and emits.
+const forwardedPropsEmits = useForwardPropsEmits(props, emits)
+</script>
+
+<template>
+  <TooltipRoot
+    v-slot="slotProps"
+    data-centoui-slot="tooltip-root"
+    v-bind="forwardedPropsEmits"
+  >
+    <slot v-bind="slotProps" />
+  </TooltipRoot>
+</template>
