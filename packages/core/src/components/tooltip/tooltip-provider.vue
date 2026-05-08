e8d349f883ec028b837d90ad3231caafde49d1fa
Favour Emeka <favorodera@gmail.com>
2026-05-08 11:36:38 +0100

    feat: add TooltipProvider component - Wraps the reka-ui TooltipProvider. - Forwards all props and slots. - Sets delayDuration default to 0.

---
+++ packages/core/src/components/tooltip/tooltip-provider.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { TooltipProvider, useForwardProps } from 'reka-ui'
+import { type TooltipProviderProps, type TooltipProviderSlots } from './index'
+
+defineOptions({
+  inheritAttrs: false,
+})
+
+defineSlots<TooltipProviderSlots>()
+
+const props = withDefaults(defineProps<TooltipProviderProps>(), {
+  delayDuration: 0,
+})
+
+// Forward props.
+const forwardedProps = useForwardProps(props)
+</script>
+
+<template>
+  <TooltipProvider
+    v-bind="forwardedProps"
+  >
+    <slot />
+  </TooltipProvider>
+</template>
