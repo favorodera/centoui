ea847ae157be364151680fffdfb66e867079c112
Favour Emeka <favorodera@gmail.com>
2026-05-10 20:52:26 +0100

    feat: Implement controllable alert open state
    - Add `open` v-model
    - Add `state` computed property
    - Add `onClose` function
    - Forward `open` prop
    - Conditionally render alert

--- packages/core/src/components/alert/alert-root.vue
+++ packages/core/src/components/alert/alert-root.vue
@@ -2,9 +2,10 @@
 import { reactiveOmit } from '@vueuse/core'
 import { useForwardProps, Primitive } from 'reka-ui'
 import { computed, reactive, toRef } from 'vue'
-import { type AlertRootSlots, type AlertRootProps, alertVariants, provideAlertRootContext } from '.'
+import { type AlertRootSlots, type AlertRootProps, alertVariants, provideAlertRootContext, type AlertRootEmits } from '.'
 
 
+defineEmits<AlertRootEmits>()
 defineSlots<AlertRootSlots>()
 
 const props = withDefaults(defineProps<AlertRootProps>(), {
@@ -15,10 +16,11 @@ const props = withDefaults(defineProps<AlertRootProps>(), {
 })
 
 // Forward props.
-const delegatedProps = reactiveOmit(props, 'class', 'orientation', 'color', 'variant')
+const delegatedProps = reactiveOmit(props, 'class', 'orientation', 'color', 'variant', 'open')
 const forwardedProps = useForwardProps(delegatedProps)
 
 // Models
+const open = defineModel<boolean>('open', { default: true })
 
 // Style class string for the component.
 const styles = computed(() => {
@@ -31,22 +33,28 @@ const styles = computed(() => {
   return root({ class: props.class })
 })
 
+// Computations
+const state = computed(() => open.value ? 'open' : 'closed')
+
 // Provide context to child components.
-provideAlertRootContext(
-  reactive({
-    variant: toRef(props, 'variant'),
-    color: toRef(props, 'color'),
-    orientation: toRef(props, 'orientation'),
-  }),
-)
+provideAlertRootContext(reactive({
+  variant: toRef(props, 'variant'),
+  color: toRef(props, 'color'),
+  orientation: toRef(props, 'orientation'),
+  state,
+  open,
+  onClose: () => open.value = false,
+}))
 </script>
 
 <template>
   <Primitive
+    v-if="open"
     data-centoui-slot="alert-root"
     :data-centoui-variant="variant"
     :data-centoui-color="color"
     :data-centoui-orientation="orientation"
+    :data-centoui-state="state"
     role="alert"
     v-bind="forwardedProps"
     :class="styles"
