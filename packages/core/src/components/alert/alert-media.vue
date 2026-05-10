ded1fd97b26e6ee2b5a4c1ce18029d73a3363d27
Favour Emeka <favorodera@gmail.com>
2026-05-10 09:14:36 +0100

    feat: add AlertMedia component - New component for alert media content. - Integrates with alert context for styling. - Provides a slot for custom media.

---
+++ packages/core/src/components/alert/alert-media.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
+import { useForwardProps, Primitive } from 'reka-ui'
+import { computed } from 'vue'
+import { type AlertMediaSlots, type AlertMediaProps, injectAlertRootContext, alertVariants } from '.'
+
+
+defineSlots<AlertMediaSlots>()
+
+const props = withDefaults(defineProps<AlertMediaProps>(), {
+  as: 'div',
+})
+
+// Forward props.
+const delegatedProps = reactiveOmit(props, 'class')
+const forwardedProps = useForwardProps(delegatedProps)
+
+// Inject AlertRoot's context
+const alertContext = injectAlertRootContext()
+
+// Style class string for the component.
+const styles = computed(() => {
+  const { media } = alertVariants({
+    color: alertContext.color,
+    variant: alertContext.variant,
+    orientation: alertContext.orientation,
+  })
+
+  return media({ class: props.class })
+})
+</script>
+
+<template>
+  <Primitive
+    data-centoui-slot="alert-media"
+    v-bind="forwardedProps"
+    :data-centoui-variant="alertContext.variant"
+    :data-centoui-color="alertContext.color"
+    :data-centoui-orientation="alertContext.orientation"
+    :class="styles"
+  >
+    <slot />
+  </Primitive>
+</template>
