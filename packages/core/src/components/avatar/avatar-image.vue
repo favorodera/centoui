d3476393e8602fb3ec1167921d7a72dd679ac75b
Favour Emeka <favorodera@gmail.com>
2026-05-12 19:39:22 +0100

    refactor(avatar): use root context for styles
    - Inject root context instead of local computed styles
    - Pass class prop to root context style function

--- packages/core/src/components/avatar/avatar-image.vue
+++ packages/core/src/components/avatar/avatar-image.vue
@@ -1,23 +1,19 @@
 <script setup lang="ts">
 import { reactiveOmit } from '@vueuse/core'
 import { AvatarImage, useForwardPropsEmits } from 'reka-ui'
-import { computed } from 'vue'
-import { avatarVariants, type AvatarImageEmits, type AvatarImageProps } from '.'
+import {
+  type AvatarImageEmits,
+  type AvatarImageProps,
+  injectCentouiAvatarRootContext,
+} from '.'
+
+const rootContext = injectCentouiAvatarRootContext()
 
 const emits = defineEmits<AvatarImageEmits>()
 
 const props = defineProps<AvatarImageProps>()
-
-// Forward props and emits.
 const delegatedProps = reactiveOmit(props, 'class')
 const forwardedPropsEmits = useForwardPropsEmits(delegatedProps, emits)
-
-// Style class string for the component.
-const styles = computed(() => {
-  const { image } = avatarVariants()
-
-  return image({ class: props.class })
-})
 </script>
 
 <template>
@@ -25,6 +21,6 @@ const styles = computed(() => {
     data-centoui-slot="avatar-image"
     :src
     v-bind="forwardedPropsEmits"
-    :class="styles"
+    :class="rootContext.styles.image({ class: props.class })"
   />
 </template>
