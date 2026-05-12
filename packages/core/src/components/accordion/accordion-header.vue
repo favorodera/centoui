4199a83460dfc604cf8cc2ef2c858151cbd05e70
Favour Emeka <favorodera@gmail.com>
2026-05-12 17:48:59 +0100

    refactor(accordion): centralize header styles and context
    - Use root context for header styles
    - Simplify data state and disabled props
    - Adjust context injection logic

--- packages/core/src/components/accordion/accordion-header.vue
+++ packages/core/src/components/accordion/accordion-header.vue
@@ -1,46 +1,33 @@
 <script setup lang="ts">
 import { reactiveOmit } from '@vueuse/core'
 import { AccordionHeader, injectAccordionItemContext, injectAccordionRootContext, useForwardProps } from 'reka-ui'
-import { computed } from 'vue'
 import {
-  accordionVariants,
+  injectCentouiAccordionRootContext,
   type AccordionHeaderProps,
   type AccordionHeaderSlots,
 } from '.'
+import { computed } from 'vue'
+
+const rootContext = computed(() => {
+  return { ...injectAccordionRootContext(), ...injectCentouiAccordionRootContext() }
+})
+const itemContext = injectAccordionItemContext()
 
 defineSlots<AccordionHeaderSlots>()
 
 const props = defineProps<AccordionHeaderProps>()
-
-// Forward props.
 const delegatedProps = reactiveOmit(props, 'class')
 const forwardedProps = useForwardProps(delegatedProps)
-
-// Style class string for the component.
-const styles = computed(() => {
-  const { header } = accordionVariants()
-
-  return header({ class: props.class })
-})
-
-// Inject AccordionRoot's and AccordionItem's context.
-const rootContext = injectAccordionRootContext()
-const itemContext = injectAccordionItemContext()
-
-// Computations
-// itemContext.disabled already factors in root-level disabled.
-const disabled = computed(() => itemContext.disabled.value ? '' : undefined)
-const state = computed(() => itemContext.dataState.value)
 </script>
 
 <template>
   <AccordionHeader
     data-centoui-slot="accordion-header"
-    :data-centoui-state="state"
+    :data-centoui-state="itemContext.dataState.value"
     :data-centoui-orientation="rootContext.orientation"
-    :data-centoui-disabled="disabled"
+    :data-centoui-disabled="itemContext.dataDisabled.value"
     v-bind="forwardedProps"
-    :class="styles"
+    :class="rootContext.styles.header({ class: props.class })"
   >
     <slot />
   </AccordionHeader>
