90bb44a02c767a6190ab3afa244982c1cedaea4e
Favour Emeka <favorodera@gmail.com>
2026-05-12 17:46:46 +0100

    style(preview): format ViewContainer classes

--- apps/preview/src/components/view-container.vue
+++ apps/preview/src/components/view-container.vue
@@ -1,9 +1,10 @@
 <template>
   <div
     class="
-      relative flex h-full flex-1 md:max-w-4xl flex-wrap content-center
-      items-center justify-center gap-8 bg-background p-3
+      relative flex h-full flex-1 flex-wrap content-center items-center
+      justify-center gap-8 bg-background p-3
       sm:p-6
+      md:max-w-4xl
     "
   >
     <slot />
