51a3a7ff5788819fe6e4409e689ec43dba9a98bf
Favour Emeka <favorodera@gmail.com>
2026-05-12 19:40:02 +0100

    feat(avatar): improve root component implementation
    - Use provideCentouiAvatarRootContext
    - Simplify computed styles
    - Pass class to root variant

--- packages/core/src/components/avatar/avatar-root.vue
+++ packages/core/src/components/avatar/avatar-root.vue
@@ -1,35 +1,32 @@
 <script setup lang="ts">
 import { reactiveOmit } from '@vueuse/core'
 import { AvatarRoot, useForwardProps } from 'reka-ui'
-import { computed } from 'vue'
-import { avatarVariants, type AvatarRootProps, type AvatarRootSlots } from '.'
+import { computed, reactive, toRef } from 'vue'
+import { avatarVariants, provideCentouiAvatarRootContext, type AvatarRootProps, type AvatarRootSlots } from '.'
 
 defineSlots<AvatarRootSlots>()
 
 const props = withDefaults(defineProps<AvatarRootProps>(), {
   size: 'md',
 })
-
-// Forward props.
 const delegatedProps = reactiveOmit(props, 'class', 'size')
 const forwardedProps = useForwardProps(delegatedProps)
 
-// Style class string for the component.
-const styles = computed(() => {
-  const { root } = avatarVariants({
-    size: props.size,
-  })
+const styles = computed(() => avatarVariants({
+  size: props.size,
+}))
 
-  return root({ class: props.class })
-})
+provideCentouiAvatarRootContext(reactive({
+  styles,
+  size: toRef(props, 'size'),
+}))
 </script>
-
 <template>
   <AvatarRoot
     data-centoui-slot="avatar-root"
     :data-centoui-size="size"
     v-bind="forwardedProps"
-    :class="styles"
+    :class="styles.root({ class: props.class })"
   >
     <slot />
   </AvatarRoot>
