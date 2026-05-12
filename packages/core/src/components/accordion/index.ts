a385351ca4db2e63f0da5be6469a380c045c4354
Favour Emeka <favorodera@gmail.com>
2026-05-12 17:52:59 +0100

    feat(accordion): add root context and refine styles
    - Introduce root context for component
    - Update trigger and item styling
    - Add icon slot for trigger
    - Refine content slot selectors

--- packages/core/src/components/accordion/index.ts
+++ packages/core/src/components/accordion/index.ts
@@ -1,11 +1,12 @@
-import type {
-  AcceptableValue,
-  AccordionContentProps as RekaAccordionContentProps,
-  AccordionHeaderProps as RekaAccordionHeaderProps,
-  AccordionItemProps as RekaAccordionItemProps,
-  AccordionRootEmits as RekaAccordionRootEmits,
-  AccordionRootProps as RekaAccordionRootProps,
-  AccordionTriggerProps as RekaAccordionTriggerProps,
+import {
+  createContext,
+  type AcceptableValue,
+  type AccordionContentProps as RekaAccordionContentProps,
+  type AccordionHeaderProps as RekaAccordionHeaderProps,
+  type AccordionItemProps as RekaAccordionItemProps,
+  type AccordionRootEmits as RekaAccordionRootEmits,
+  type AccordionRootProps as RekaAccordionRootProps,
+  type AccordionTriggerProps as RekaAccordionTriggerProps,
 } from 'reka-ui'
 import { tv, type ClassValue, type VariantProps } from 'tailwind-variants'
 
@@ -14,22 +15,25 @@ export const accordionVariants = tv({
   slots: {
     root: 'w-full',
     item: `
-      border-b border-b-muted
+      border-b border-b-border text-sm
       last:border-b-0
     `,
     header: 'flex flex-wrap',
     trigger: `
-      flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-start
-      text-sm font-medium transition-all outline-none
-      hover:text-foreground/70
-      focus-visible:ring-2 focus-visible:ring-muted focus-visible:ring-offset-2
-      disabled:pointer-events-none disabled:opacity-50
-      [&[data-centoui-state=open]>svg]:rotate-180
+      flex flex-1 cursor-pointer items-start justify-between gap-4 py-4
+      font-medium
+      focus-visible:ring-2 focus-visible:ring-primary
+      focus-visible:ring-offset-2
+      data-centoui-disabled:pointer-events-none data-centoui-disabled:opacity-50
+      [&[data-centoui-state=open]>[data-centoui-slot="accordion-trigger-icon"]]:rotate-180
+    `,
+    triggerIcon: `
+      pointer-events-none size-4 shrink-0 translate-y-0.5 text-muted-foreground
+      transition-transform
     `,
     content: `
-      overflow-hidden text-sm
-      *:data-[centoui-slot="accordion-content-body"]:pt-0
-      *:data-[centoui-slot="accordion-content-body"]:pb-4
+      overflow-hidden
+      *:data-[centoui-slot="accordion-content-inner"]:pb-4
       data-[centoui-state=closed]:animate-accordion-up
       data-[centoui-state=open]:animate-accordion-down
     `,
@@ -37,7 +41,7 @@ export const accordionVariants = tv({
 })
 
 
-// Component exports
+// COMPONENTS
 
 export { default as AccordionContent } from './accordion-content.vue'
 export { default as AccordionHeader } from './accordion-header.vue'
@@ -46,7 +50,18 @@ export { default as AccordionRoot } from './accordion-root.vue'
 export { default as AccordionTrigger } from './accordion-trigger.vue'
 
 
-// Types exports
+// CONTEXTS
+
+export const [injectCentouiAccordionRootContext, provideCentouiAccordionRootContext] = createContext<AccordionRootContext>('AccordionRoot', 'centoui:accordion-root:context')
+
+
+// PROPS
+
+/** AccordionRoot context type */
+export type AccordionRootContext = {
+  /** Styles for the alert */
+  styles: ReturnType<typeof accordionVariants>
+}
 
 /** Variant props for Accordion component. */
 export type AccordionVariants = VariantProps<typeof accordionVariants>
@@ -81,8 +96,14 @@ export type AccordionContentProps = RekaAccordionContentProps & {
   class?: ClassValue
 }
 
+
+// EMITS
+
 /** Emits for AccordionRoot component. */
 export type AccordionRootEmits = RekaAccordionRootEmits
+
+
+// SLOTS
 
 /** Slots for AccordionRoot component. */
 export type AccordionRootSlots = {
@@ -112,6 +133,8 @@ export type AccordionHeaderSlots = {
 export type AccordionTriggerSlots = {
   /** Default slot. */
   default: []
+  /** Icon slot. */
+  icon: []
 }
 
 /** Slots for AccordionContent component. */
