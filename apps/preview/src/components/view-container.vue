d50e19443c6b389524b5ab92cbb5c04691060fca
Favour Emeka <favorodera@gmail.com>
2026-05-12 17:21:03 +0100

    style(preview): set max-width on view container

--- apps/preview/src/components/view-container.vue
+++ apps/preview/src/components/view-container.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     class="
-      relative flex h-full flex-1 flex-wrap content-center
+      relative flex h-full flex-1 md:max-w-4xl flex-wrap content-center
       items-center justify-center gap-8 bg-background p-3
       sm:p-6
     "
