ce6aba1305e208c2dec4efead8179e8051b540f6
Favour Emeka <favorodera@gmail.com>
2026-05-14 22:59:48 +0100

    refactor(preview): simplify Separator view config**
    - Update usePreview hook integration
    - Remove redundant prop definitions
    - Use alias paths for imports

--- apps/preview/src/views/separator-view.vue
+++ apps/preview/src/views/separator-view.vue
@@ -1,67 +1,32 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
 import { Separator, separatorVariants } from '#centoui/components/separator'
 import { Icon } from '@iconify/vue'
-import { usePreview } from '../utils/use-preview'
-import type { PropsSchema } from '../components/props-panel.vue'
-import ViewContainer from '../components/view-container.vue'
+import ViewContainer from '@/components/view-container.vue'
+import { usePreview } from '@/composables/use-preview'
 
-const { values, setPreviewState } = usePreview()
-
-const schema: PropsSchema = {
-  size: {
-    type: 'select',
-    label: 'Size',
-    options: Object.keys(separatorVariants.variants.size),
-    default: separatorVariants.defaultVariants.size,
-  },
-  color: {
-    type: 'select',
-    label: 'Color',
-    options: Object.keys(separatorVariants.variants.color),
-    default: separatorVariants.defaultVariants.color,
-  },
-  variant: {
-    type: 'select',
-    label: 'Variant',
-    options: Object.keys(separatorVariants.variants.variant),
-    default: separatorVariants.defaultVariants.variant,
-  },
+const values = usePreview('Separator', {
   orientation: {
     type: 'select',
     label: 'Orientation',
     options: Object.keys(separatorVariants.variants.orientation),
     default: separatorVariants.defaultVariants.orientation,
   },
-}
-
-onMounted(() => {
-  setPreviewState('Separator', schema)
 })
 </script>
 
 <template>
   <ViewContainer>
     <Separator
-      :size="values.size"
-      :color="values.color"
-      :variant="values.variant"
       :orientation="values.orientation"
     />
 
     <Separator
-      :size="values.size"
-      :color="values.color"
-      :variant="values.variant"
       :orientation="values.orientation"
     >
       <Icon icon="lucide:user" />
     </Separator>
 
     <Separator
-      :size="values.size"
-      :color="values.color"
-      :variant="values.variant"
       :orientation="values.orientation"
     >
       Hello
