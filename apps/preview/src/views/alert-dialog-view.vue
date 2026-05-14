626fca5b4babd818862930721f3643be719a8a5e
Favour Emeka <favorodera@gmail.com>
2026-05-14 22:57:13 +0100

    feat(preview): add AlertDialog view
    - Adds new view for AlertDialog
    - Showcases basic component usage
    - Imports AlertDialog elements
    - Includes trigger button and overlay
    - Demonstrates title and description

---
+++ apps/preview/src/views/alert-dialog-view.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import {
+  AlertDialogRoot,
+  AlertDialogTrigger,
+  AlertDialogPortal,
+  AlertDialogOverlay,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogCancel,
+  AlertDialogAction,
+} from '#centoui/components/alert-dialog'
+import { Button } from '#centoui/components/button'
+import ViewContainer from '@/components/view-container.vue'
+import { usePreview } from '@/composables/use-preview'
+
+usePreview('AlertDialog', {})
+</script>
+
+<template>
+  <ViewContainer class="w-lg">
+    <AlertDialogRoot>
+
+      <AlertDialogTrigger as-child>
+        <Button>
+          Open Alert Dialog
+        </Button>
+      </AlertDialogTrigger>
+
+      <AlertDialogPortal>
+        <AlertDialogOverlay />
+
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+
+        </AlertDialogContent>
+
+      </AlertDialogPortal>
+    </AlertDialogRoot>
+  </ViewContainer>
+</template>
