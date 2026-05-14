0a9e944d2393e3c0c810ca2a947657a0ad1ef4e9
Favour Emeka <favorodera@gmail.com>
2026-05-14 23:01:20 +0100

    refactor(accordion): simplify content component context
    - Removed redundant context injection
    - Updated data attribute naming
    - Streamlined prop forwarding

--- packages/core/src/components/accordion/accordion-content.vue
+++ packages/core/src/components/accordion/accordion-content.vue
@@ -1,17 +1,13 @@
 <script setup lang="ts">
 import { reactiveOmit } from '@vueuse/core'
-import { AccordionContent, injectAccordionItemContext, injectAccordionRootContext, useForwardProps } from 'reka-ui'
+import { AccordionContent, useForwardProps } from 'reka-ui'
 import {
   type AccordionContentProps,
   type AccordionContentSlots,
   injectCentouiAccordionRootContext,
 } from '.'
-import { computed } from 'vue'
 
-const rootContext = computed(() => {
-  return { ...injectAccordionRootContext(), ...injectCentouiAccordionRootContext() }
-})
-const itemContext = injectAccordionItemContext()
+const rootContext = injectCentouiAccordionRootContext()
 
 defineSlots<AccordionContentSlots>()
 
@@ -22,14 +18,11 @@ const forwardedProps = useForwardProps(delegatedProps)
 
 <template>
   <AccordionContent
-    data-centoui-slot="accordion-content"
-    :data-centoui-state="itemContext.dataState.value"
-    :data-centoui-orientation="rootContext.orientation"
-    :data-centoui-disabled="itemContext.dataDisabled.value"
+    data-slot="accordion-content"
     v-bind="forwardedProps"
     :class="rootContext.styles.content({ class: props.class })"
   >
-    <div data-centoui-slot="accordion-content-inner">
+    <div data-slot="accordion-content-inner">
       <slot />
     </div>
   </AccordionContent>
