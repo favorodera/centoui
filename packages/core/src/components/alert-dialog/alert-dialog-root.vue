6bd3db03b8b5fff4aacae084d3e58b4eafa57d08
Favour Emeka <favorodera@gmail.com>
2026-05-15 03:09:32 +0100

    feat(alert-dialog): add alert dialog root component
    - Implemented AlertDialogRoot component
    - Integrated reka-ui and centoui
    - Provided context for variants and styles

---
+++ packages/core/src/components/alert-dialog/alert-dialog-root.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { AlertDialogRoot, useForwardPropsEmits } from 'reka-ui'
+import { type AlertDialogRootSlots, type AlertDialogRootProps, type AlertDialogRootEmits, provideCentouiAlertDialogRootContext, alertDialogVariants } from '.'
+import { computed, reactive } from 'vue'
+
+defineSlots<AlertDialogRootSlots>()
+
+const emits = defineEmits<AlertDialogRootEmits>()
+
+const props = defineProps<AlertDialogRootProps>()
+  
+const forwardedPropsEmits = useForwardPropsEmits(props, emits)
+
+const styles = computed(() => alertDialogVariants())
+
+provideCentouiAlertDialogRootContext(reactive({
+  styles,
+}))
+</script>
+
+<template>
+  <AlertDialogRoot
+    v-slot="slotProps"
+    v-bind="forwardedPropsEmits"
+    data-slot="alert-dialog-root"
+  >
+    <slot v-bind="slotProps" />
+  </AlertDialogRoot>
+</template>
