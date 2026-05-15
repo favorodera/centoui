789bc9460e9dd3ab2013f9c2a0d7aac70b5fe3bf
Favour Emeka <favorodera@gmail.com>
2026-05-15 03:18:55 +0100

    fix(tooltip-trigger): refactor to use Centoui context and clean up unused imports and code

--- packages/core/src/components/tooltip/tooltip-trigger.vue
+++ packages/core/src/components/tooltip/tooltip-trigger.vue
@@ -1,37 +1,22 @@
 <script setup lang="ts">
 import { reactiveOmit } from '@vueuse/core'
-import { injectTooltipRootContext, TooltipTrigger, useForwardProps } from 'reka-ui'
-import { computed } from 'vue'
-import { tooltipVariants, type TooltipTriggerProps, type TooltipTriggerSlots } from './index'
+import { TooltipTrigger, useForwardProps } from 'reka-ui'
+import { injectCentouiTooltipRootContext, type TooltipTriggerProps, type TooltipTriggerSlots } from './index'
+
+const rootContext = injectCentouiTooltipRootContext()
 
 defineSlots<TooltipTriggerSlots>()
 
 const props = defineProps<TooltipTriggerProps>()
-
-// Forward props.
 const delegatedProps = reactiveOmit(props, 'class')
 const forwardedProps = useForwardProps(delegatedProps)
-
-// Style class string for the component.
-const styles = computed(() => {
-  const { trigger } = tooltipVariants({})
-
-  return trigger({ class: props.class })
-})
-
-// Inject TooltipRoot's context
-const tooltipRootContext = injectTooltipRootContext()
-
-// Computations
-const state = computed(() => tooltipRootContext.stateAttribute.value)
 </script>
 
 <template>
   <TooltipTrigger
-    data-centoui-slot="tooltip-trigger"
-    :data-centoui-state="state"
+    data-slot="tooltip-trigger"
     v-bind="forwardedProps"
-    :class="styles"
+    :class="rootContext.styles.trigger({ class: props.class })"
   >
     <slot />
   </TooltipTrigger>
