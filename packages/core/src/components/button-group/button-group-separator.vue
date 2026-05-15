611eb7aad71a55dd09488e31601de53a9cda7e37
Favour Emeka <favorodera@gmail.com>
2026-05-15 03:15:01 +0100

    fix(button-group-separator): update context injection and refine orientation handling

--- packages/core/src/components/button-group/button-group-separator.vue
+++ packages/core/src/components/button-group/button-group-separator.vue
@@ -1,33 +1,21 @@
 <script setup lang="ts">
 import { reactiveOmit } from '@vueuse/core'
 import { computed, useAttrs } from 'vue'
-import { buttonGroupVariants, injectButtonGroupContext, type ButtonGroupSeparatorProps } from '.'
+import { injectCentouiButtonGroupContext, type ButtonGroupSeparatorProps } from '.'
 import { Separator } from '../separator'
 
 defineOptions({
   inheritAttrs: false,
 })
 
+const buttonGroupContext = injectCentouiButtonGroupContext()
+
 const attributes = useAttrs()
 
 const props = defineProps<ButtonGroupSeparatorProps>()
-
-// Delegate props.
 const delegatedProps = reactiveOmit(props, 'class')
 
-// Style class string for the component.
-const styles = computed(() => {
-  const { separator } = buttonGroupVariants()
-  
-  return separator({ class: props.class })
-})
-
-// Inject ButtonGroup's context
-const buttonGroupContext = injectButtonGroupContext()
-
-// Computations
-const orientation = computed(() => {
-  // Flip orientation so the separator is perpendicular to the group's layout direction.
+const flippedOrientation = computed(() => {
   return buttonGroupContext.orientation === 'vertical' ? 'horizontal' : 'vertical'
 })
 </script>
@@ -36,7 +24,7 @@ const orientation = computed(() => {
   <Separator
     data-centoui-slot="button-group-separator"
     v-bind="{...delegatedProps, ...attributes}"
-    :orientation="orientation"
-    :class="styles"
+    :orientation="flippedOrientation"
+    :class="buttonGroupContext.styles.separator({class:props.class})"
   />
 </template>
