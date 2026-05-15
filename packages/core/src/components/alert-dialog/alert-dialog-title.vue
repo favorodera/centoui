28ea75eb4989e85166fd2f9243dfc852275c4956
Favour Emeka <favorodera@gmail.com>
2026-05-15 03:09:44 +0100

    feat(alert-dialog): add alert dialog title component
    - Implemented AlertDialogTitle component
    - Added slot and prop forwarding
    - Integrated with root context styles

---
+++ packages/core/src/components/alert-dialog/alert-dialog-title.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
+import { AlertDialogTitle, useForwardPropsEmits } from 'reka-ui'
+import {
+  type AlertDialogTitleProps,
+  type AlertDialogTitleSlots,
+  injectCentouiAlertDialogRootContext,
+} from '.'
+
+const rootContext = injectCentouiAlertDialogRootContext()
+
+defineSlots<AlertDialogTitleSlots>()
+
+const props = defineProps<AlertDialogTitleProps>()
+const delegatedProps = reactiveOmit(props, 'class')
+const forwardedProps = useForwardPropsEmits(delegatedProps)
+</script>
+
+<template>
+  <AlertDialogTitle
+    data-slot="alert-dialog-title"
+    v-bind="forwardedProps"
+    :class="rootContext.styles.title({ class: props.class })"
+  >
+    <slot />
+  </AlertDialogTitle>
+</template>
