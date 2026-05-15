28033af860b27898a4bf89d16111023eec91c859
Favour Emeka <favorodera@gmail.com>
2026-05-15 03:15:27 +0100

    fix(button-group): refine style variants and update context exports

--- packages/core/src/components/button-group/index.ts
+++ packages/core/src/components/button-group/index.ts
@@ -2,22 +2,20 @@ import { createContext, type PrimitiveProps } from 'reka-ui'
 import { tv, type ClassValue, type VariantProps } from 'tailwind-variants'
 import type { SeparatorProps } from '../separator'
 
-/** ButtonGroup style variants */
 export const buttonGroupVariants = tv({
   slots: {
     root: `
       relative flex w-fit items-stretch
-      *:focus-visible:relative *:focus-visible:z-10
-      has-[>[data-centoui-slot=button-group]]:gap-2
-      has-[select[aria-hidden=true]:last-child]:[&>[data-centoui-slot=select-trigger]:last-of-type]:rounded-r-md
-      [&>[data-centoui-slot=select-trigger]:not([class*=\'w-\'])]:w-fit
+      *:focus-visible:z-10
+      has-[>[data-slot=button-group]]:gap-2
+      has-[select[aria-hidden=true]:last-child]:[&>[data-slot=select-trigger]:last-of-type]:rounded-r-md
+      [&>[data-slot=select-trigger]:not([class*=\'w-\'])]:w-fit
       [&>input]:flex-1
     `,
     separator: 'm-0 gap-0 self-stretch',
   },
   variants: {
     orientation: {
-      // Vertical
       vertical: {
         root: `
           flex-col
@@ -27,7 +25,6 @@ export const buttonGroupVariants = tv({
         `,
         separator: '',
       },
-      // Horizontal
       horizontal: {
         root: `
           [&>*:not(:first-child)]:rounded-l-none
@@ -44,26 +41,32 @@ export const buttonGroupVariants = tv({
 })
 
 
-// Component exports
+// COMPONENTS
 
 export { default as ButtonGroupSeparator } from './button-group-separator.vue'
 export { default as ButtonGroup } from './button-group.vue'
 
 
-// Methods exports
+// CONTEXT
 
-export const [injectButtonGroupContext, provideButtonGroupContext] = createContext<ButtonGroupContext>('ButtonGroup', 'centoui:button-group:context')
+export const [injectCentouiButtonGroupContext, provideCentouiButtonGroupContext] = createContext<ButtonGroupContext>('ButtonGroup', 'centoui:button-group:context')
 
 
-// Types exports
+// TYPES — Variants
 
-/** ButtonGroup context type */
-export type ButtonGroupContext = Pick<ButtonGroupVariants, 'orientation'>
-
-/** Variant props for ButtonGroup component. */
 export type ButtonGroupVariants = VariantProps<typeof buttonGroupVariants>
 
-/** Props for ButtonGroup component. */
+
+// TYPES — Context
+
+export type ButtonGroupContext = Pick<ButtonGroupVariants, 'orientation'> & {
+  /** Styles for the button group */
+  styles: ReturnType<typeof buttonGroupVariants>
+}
+
+
+// TYPES — Props
+
 export type ButtonGroupProps = PrimitiveProps & {
   /**
    * The orientation of the button group.
@@ -74,14 +77,11 @@ export type ButtonGroupProps = PrimitiveProps & {
   class?: ClassValue
 }
 
-/** Props for ButtonGroupSeparator component. */
-export type ButtonGroupSeparatorProps = Pick<SeparatorProps, 'color' | 'size' | 'variant' | 'decorative'> & {
-  /** Custom class to apply to the root element. */
-  class?: ClassValue
-}
+export type ButtonGroupSeparatorProps = SeparatorProps
 
-/** Slots for ButtonGroup component. */
+
+// TYPES — Slots
+
 export type ButtonGroupSlots = {
-  /** Default slot. */
   default: []
 }
