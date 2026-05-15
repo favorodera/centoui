c5521c03b8c91562d9b0bbf2ca9d634d6e1b42c8
Favour Emeka <favorodera@gmail.com>
2026-05-15 03:09:02 +0100

    feat(alert-dialog): add portal component
    - Implemented AlertDialogPortal component
    - Utilized reka-ui for forwarding props
    - Added slots for content

---
+++ packages/core/src/components/alert-dialog/alert-dialog-portal.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useForwardProps, AlertDialogPortal } from 'reka-ui'
+import {
+  type AlertDialogPortalSlots,
+  type AlertDialogPortalProps,
+} from '.'
+
+defineSlots<AlertDialogPortalSlots>()
+
+const props = defineProps<AlertDialogPortalProps>()
+const forwardedProps = useForwardProps(props)
+</script>
+
+<template>
+  <AlertDialogPortal
+    v-bind="forwardedProps"
+  >
+    <slot />
+  </AlertDialogPortal>
+</template>
