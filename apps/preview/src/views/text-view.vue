85cc80dd6de6ea4434c474222659be2a843a69ca
Favour Emeka <favorodera@gmail.com>
2026-05-16 10:19:25 +0100

    feat(preview/text): add Text component preview - Implements new view for Text component - Adds interactive controls for size and weight

---
+++ apps/preview/src/views/text-view.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { Text, textVariants } from '#centoui/components/text'
+import ViewContainer from '@/components/view-container.vue'
+import { usePreview } from '@/composables/use-preview'
+
+const values = usePreview('Text', {
+  size: {
+    type: 'select',
+    label: 'Size',
+    options: Object.keys(textVariants.variants.size),
+    default: textVariants.defaultVariants.size,
+  },
+  weight: {
+    type: 'select',
+    label: 'Weight',
+    options: Object.keys(textVariants.variants.weight),
+    default: textVariants.defaultVariants.weight,
+  },
+})
+</script>
+
+<template>
+  <ViewContainer>
+    <Text
+      :size="values.size"
+      :weight="values.weight"
+    >
+      Hello World
+    </Text>
+  </ViewContainer>
+</template>
