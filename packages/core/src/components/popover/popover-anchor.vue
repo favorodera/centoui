2c8d5fb8e72365d1a6df3097847df95ddd130d24
Favour Emeka <favorodera@gmail.com>
2026-05-17 00:36:33 +0100

    feat(popover): add PopoverAnchor component

---
+++ packages/core/src/components/popover/popover-anchor.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useForwardProps, PopoverAnchor } from 'reka-ui'
+import {
+  type PopoverAnchorSlots,
+  type PopoverAnchorProps,
+} from '.'
+
+defineSlots<PopoverAnchorSlots>()
+
+const props = defineProps<PopoverAnchorProps>()
+const forwardedProps = useForwardProps(props)
+</script>
+
+<template>
+  <PopoverAnchor
+    data-slot="popover-anchor"
+    v-bind="forwardedProps"
+  >
+    <slot />
+  </PopoverAnchor>
+</template>
