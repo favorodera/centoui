9ee5e5e3b6c9af4021e83c68b6d6ef93ff4b8e99
Favour Emeka <favorodera@gmail.com>
2026-05-17 00:49:05 +0100

    feat(popover): add PopoverRoot component

---
+++ packages/core/src/components/popover/popover-root.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { PopoverRoot, useForwardPropsEmits } from 'reka-ui'
+import {
+  type PopoverRootSlots,
+  type PopoverRootProps,
+  type PopoverRootEmits,
+  provideCentouiPopoverRootContext,
+  popoverVariants,
+} from '.'
+import { computed, reactive } from 'vue'
+
+defineSlots<PopoverRootSlots>()
+
+const emits = defineEmits<PopoverRootEmits>()
+
+const props = defineProps<PopoverRootProps>()
+  
+const forwardedPropsEmits = useForwardPropsEmits(props, emits)
+
+const styles = computed(() => popoverVariants())
+
+provideCentouiPopoverRootContext(reactive({
+  styles,
+}))
+</script>
+
+<template>
+  <PopoverRoot
+    v-slot="slotProps"
+    v-bind="forwardedPropsEmits"
+    data-slot="popover-root"
+  >
+    <slot v-bind="slotProps" />
+  </PopoverRoot>
+</template>
