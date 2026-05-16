dcaab6cd5f6c76b0d6bc6a46aec12104b22e264c
Favour Emeka <favorodera@gmail.com>
2026-05-17 00:48:18 +0100

    feat(popover): create PopoverPortal component

---
+++ packages/core/src/components/popover/popover-portal.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useForwardProps, PopoverPortal } from 'reka-ui'
+import {
+  type PopoverPortalSlots,
+  type PopoverPortalProps,
+} from '.'
+
+defineSlots<PopoverPortalSlots>()
+
+const props = defineProps<PopoverPortalProps>()
+const forwardedProps = useForwardProps(props)
+</script>
+
+<template>
+  <PopoverPortal
+    v-bind="forwardedProps"
+  >
+    <slot />
+  </PopoverPortal>
+</template>
