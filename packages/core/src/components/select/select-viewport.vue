96c98107e96b11f605d861b5386644cdfd26daf4
Favour Emeka <favorodera@gmail.com>
2026-05-17 18:52:49 +0100

    feat(select): add select viewport component

---
+++ packages/core/src/components/select/select-viewport.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
+import { SelectViewport, useForwardProps } from 'reka-ui'
+import {
+  type SelectViewportProps,
+  type SelectViewportSlots,
+  injectCentouiSelectRootContext,
+} from '.'
+
+const rootContext = injectCentouiSelectRootContext()
+
+defineSlots<SelectViewportSlots>()
+
+const props = defineProps<SelectViewportProps>()
+const delegatedProps = reactiveOmit(props, 'class')
+const forwardedProps = useForwardProps(delegatedProps)
+</script>
+
+<template>
+  <SelectViewport
+    v-bind="forwardedProps"
+    data-slot="select-viewport"
+    :class="rootContext.styles.viewport({ class: props.class })"
+  >
+    <slot />
+  </SelectViewport>
+</template>
