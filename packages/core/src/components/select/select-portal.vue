08fdda5643d19c3ff7d8c5c03bffe1d203b630f3
Favour Emeka <favorodera@gmail.com>
2026-05-17 18:50:43 +0100

    feat(select): add select portal component

---
+++ packages/core/src/components/select/select-portal.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useForwardProps, SelectPortal } from 'reka-ui'
+import {
+  type SelectPortalProps,
+  type SelectPortalSlots,
+} from '.'
+
+defineSlots<SelectPortalSlots>()
+
+const props = defineProps<SelectPortalProps>()
+const forwardedProps = useForwardProps(props)
+</script>
+
+<template>
+  <SelectPortal
+    v-bind="forwardedProps"
+  >
+    <slot />
+  </SelectPortal>
+</template>
