65fe5a80c790e6abbb54e137a001709f49745c95
Favour Emeka <favorodera@gmail.com>
2026-05-18 19:39:29 +0100

    refactor(popover): use local variants for styling

--- packages/core/src/components/popover/popover-footer.vue
+++ packages/core/src/components/popover/popover-footer.vue
@@ -2,27 +2,29 @@
 import { reactiveOmit } from '@vueuse/core'
 import { useForwardProps, Primitive } from 'reka-ui'
 import {
-  type PopoverFooterSlots,
   type PopoverFooterProps,
-  injectCentouiPopoverRootContext,
+  popoverVariants,
 } from '.'
-
-const rootContext = injectCentouiPopoverRootContext()
-
-defineSlots<PopoverFooterSlots>()
+import { computed } from 'vue'
 
 const props = withDefaults(defineProps<PopoverFooterProps>(), {
   as: 'div',
 })
 const delegatedProps = reactiveOmit(props, 'class')
 const forwardedProps = useForwardProps(delegatedProps)
+
+const classNames = computed(() => {
+  const { footer } = popoverVariants()
+  
+  return footer({ class: props.class })
+})
 </script>
 
 <template>
   <Primitive
     data-slot="popover-footer"
     v-bind="forwardedProps"
-    :class="rootContext.styles.footer({ class: props.class })"
+    :class="classNames"
   >
     <slot />
   </Primitive>
