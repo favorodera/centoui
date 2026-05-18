ae80f7c8dcd69cabed58dc3c8af2c8152e748fd6
Favour Emeka <favorodera@gmail.com>
2026-05-18 19:39:36 +0100

    refactor(select): use local variants for styling

--- packages/core/src/components/select/select-item.vue
+++ packages/core/src/components/select/select-item.vue
@@ -1,6 +1,38 @@
 <script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
+import { SelectItem, useForwardPropsEmits } from 'reka-ui'
+import {
+  type SelectItemProps,
+  type SelectItemEmits,
+  injectCentouiSelectRootContext,
+  selectVariants,
+} from '.'
+import { computed } from 'vue'
+
+const rootContext = injectCentouiSelectRootContext()
+
+const emits = defineEmits<SelectItemEmits>()
+
+const props = defineProps<SelectItemProps>()
+const delegatedProps = reactiveOmit(props, 'class')
+
+const forwardedPropsEmits = useForwardPropsEmits(delegatedProps, emits)
+
+const classNames = computed(() => {
+  const { item } = selectVariants({
+    size: rootContext.size,
+  })
+  
+  return item({ class: props.class })
+})
 </script>
 
 <template>
-  <div />
+  <SelectItem
+    data-slot="select-item"
+    v-bind="forwardedPropsEmits"
+    :class="classNames"
+  >
+    <slot />
+  </SelectItem>
 </template>
