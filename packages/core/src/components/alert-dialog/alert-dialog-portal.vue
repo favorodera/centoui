1d257fd580f0e9ed044460652da2a0456a003d76
Favour Emeka <favorodera@gmail.com>
2026-05-18 19:39:09 +0100

    refactor(alert-dialog): cleanup component imports and template

--- packages/core/src/components/alert-dialog/alert-dialog-portal.vue
+++ packages/core/src/components/alert-dialog/alert-dialog-portal.vue
@@ -1,11 +1,8 @@
 <script setup lang="ts">
 import { useForwardProps, AlertDialogPortal } from 'reka-ui'
 import {
-  type AlertDialogPortalSlots,
   type AlertDialogPortalProps,
 } from '.'
-
-defineSlots<AlertDialogPortalSlots>()
 
 const props = defineProps<AlertDialogPortalProps>()
 const forwardedProps = useForwardProps(props)
