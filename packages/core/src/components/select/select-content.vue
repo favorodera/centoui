5264e1bc4360aec40756587bef5a05ea022fd40a
Favour Emeka <favorodera@gmail.com>
2026-05-18 19:39:34 +0100

    refactor(select): use local variants for styling

--- packages/core/src/components/select/select-content.vue
+++ packages/core/src/components/select/select-content.vue
@@ -1,30 +1,40 @@
 <script setup lang="ts">
 import { reactiveOmit } from '@vueuse/core'
 import { SelectContent, useForwardPropsEmits } from 'reka-ui'
+import { computed } from 'vue'
 import {
   type SelectContentEmits,
   type SelectContentProps,
-  type SelectContentSlots,
   injectCentouiSelectRootContext,
+  selectVariants,
 } from '.'
 
 const rootContext = injectCentouiSelectRootContext()
 
-defineSlots<SelectContentSlots>()
-
 const emits = defineEmits<SelectContentEmits>()
 
-const props = defineProps<SelectContentProps>()
+const props = withDefaults(defineProps<SelectContentProps>(), {
+  position: 'popper',
+})
 const delegatedProps = reactiveOmit(props, 'class')
 
 const forwardedPropsEmits = useForwardPropsEmits(delegatedProps, emits)
+
+const classNames = computed(() => {
+  const { content } = selectVariants({
+    size: rootContext.size,
+    contentPosition: props.position,
+  })
+  
+  return content({ class: props.class })
+})
 </script>
 
 <template>
   <SelectContent
     v-bind="forwardedPropsEmits"
     data-slot="select-content"
-    :class="rootContext.styles.content({class:props.class})"
+    :class="classNames"
   >
     <slot />
   </SelectContent>
