d356c34f51e9183e9cf2b78a5705b108cc170764
Favour Emeka <favorodera@gmail.com>
2026-05-18 19:39:03 +0100

    refactor(accordion): use local variants for styling

--- packages/core/src/components/accordion/accordion-header.vue
+++ packages/core/src/components/accordion/accordion-header.vue
@@ -1,26 +1,25 @@
 <script setup lang="ts">
 import { reactiveOmit } from '@vueuse/core'
 import { AccordionHeader, useForwardProps } from 'reka-ui'
-import {
-  injectCentouiAccordionRootContext,
-  type AccordionHeaderProps,
-  type AccordionHeaderSlots,
-} from '.'
-
-const rootContext = injectCentouiAccordionRootContext()
-
-defineSlots<AccordionHeaderSlots>()
+import { accordionVariants, type AccordionHeaderProps } from '.'
+import { computed } from 'vue'
 
 const props = defineProps<AccordionHeaderProps>()
 const delegatedProps = reactiveOmit(props, 'class')
 const forwardedProps = useForwardProps(delegatedProps)
+
+const classNames = computed(() => {
+  const { header } = accordionVariants()
+  
+  return header({ class: props.class })
+})
 </script>
 
 <template>
   <AccordionHeader
     data-slot="accordion-header"
     v-bind="forwardedProps"
-    :class="rootContext.styles.header({ class: props.class })"
+    :class="classNames"
   >
     <slot />
   </AccordionHeader>
