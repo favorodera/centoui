e57c8fc0813ff7d3d1ed3bffcea678f811786f0d
Favour Emeka <favorodera@gmail.com>
2026-05-18 19:39:10 +0100

    refactor(alert-dialog): use local variants for styling

--- packages/core/src/components/alert-dialog/alert-dialog-title.vue
+++ packages/core/src/components/alert-dialog/alert-dialog-title.vue
@@ -3,24 +3,26 @@ import { reactiveOmit } from '@vueuse/core'
 import { AlertDialogTitle, useForwardPropsEmits } from 'reka-ui'
 import {
   type AlertDialogTitleProps,
-  type AlertDialogTitleSlots,
-  injectCentouiAlertDialogRootContext,
+  alertDialogVariants,
 } from '.'
-
-const rootContext = injectCentouiAlertDialogRootContext()
-
-defineSlots<AlertDialogTitleSlots>()
+import { computed } from 'vue'
 
 const props = defineProps<AlertDialogTitleProps>()
 const delegatedProps = reactiveOmit(props, 'class')
 const forwardedProps = useForwardPropsEmits(delegatedProps)
+
+const classNames = computed(() => {
+  const { title } = alertDialogVariants()
+  
+  return title({ class: props.class })
+})
 </script>
 
 <template>
   <AlertDialogTitle
     data-slot="alert-dialog-title"
     v-bind="forwardedProps"
-    :class="rootContext.styles.title({ class: props.class })"
+    :class="classNames"
   >
     <slot />
   </AlertDialogTitle>
