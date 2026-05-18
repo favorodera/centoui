d0be0cb1916c1a666b946796165a8b63c172f19e
Favour Emeka <favorodera@gmail.com>
2026-05-18 19:39:08 +0100

    refactor(alert-dialog): use local variants for styling

--- packages/core/src/components/alert-dialog/alert-dialog-header.vue
+++ packages/core/src/components/alert-dialog/alert-dialog-header.vue
@@ -1,24 +1,30 @@
 <script setup lang="ts">
 import { reactiveOmit } from '@vueuse/core'
 import { useForwardProps, Primitive } from 'reka-ui'
-import { type AlertDialogHeaderProps, type AlertDialogHeaderSlots, injectCentouiAlertDialogRootContext } from '.'
-
-const rootContext = injectCentouiAlertDialogRootContext()
-
-defineSlots<AlertDialogHeaderSlots>()
+import {
+  type AlertDialogHeaderProps,
+  alertDialogVariants,
+} from '.'
+import { computed } from 'vue'
 
 const props = withDefaults(defineProps<AlertDialogHeaderProps>(), {
   as: 'div',
 })
 const delegatedProps = reactiveOmit(props, 'class')
 const forwardedProps = useForwardProps(delegatedProps)
+
+const classNames = computed(() => {
+  const { header } = alertDialogVariants()
+  
+  return header({ class: props.class })
+})
 </script>
 
 <template>
   <Primitive
     data-slot="alert-dialog-header"
     v-bind="forwardedProps"
-    :class="rootContext.styles.header({ class: props.class })"
+    :class="classNames"
   >
     <slot />
   </Primitive>
