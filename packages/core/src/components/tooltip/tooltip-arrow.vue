06fd657a6979d89c1b75bfc21973ec5157680ed5
Favour Emeka <favorodera@gmail.com>
2026-05-18 19:39:43 +0100

    refactor(tooltip): use local variants for styling

--- packages/core/src/components/tooltip/tooltip-arrow.vue
+++ packages/core/src/components/tooltip/tooltip-arrow.vue
@@ -1,23 +1,26 @@
 <script setup lang="ts">
 import { reactiveOmit } from '@vueuse/core'
 import { TooltipArrow, useForwardProps } from 'reka-ui'
-import { injectCentouiTooltipRootContext, type TooltipArrowProps, type TooltipArrowSlots } from './index'
-
-const rootContext = injectCentouiTooltipRootContext()
-
-defineSlots<TooltipArrowSlots>()
+import { computed } from 'vue'
+import { tooltipVariants, type TooltipArrowProps } from './index'
 
 const props = defineProps<TooltipArrowProps>()
 
 const delegatedProps = reactiveOmit(props, 'class')
 const forwardedProps = useForwardProps(delegatedProps)
+
+const classNames = computed(() => {
+  const { arrow } = tooltipVariants()
+
+  return arrow({ class: props.class })
+})
 </script>
 
 <template>
   <TooltipArrow
     data-slot="tooltip-arrow"
     v-bind="forwardedProps"
-    :class="rootContext.styles.arrow({ class: props.class })"
+    :class="classNames"
   >
     <slot />
   </TooltipArrow>
