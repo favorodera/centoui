65756b631ad9e11017589faafeb684286c9213c2
Favour Emeka <favorodera@gmail.com>
2026-05-18 19:39:35 +0100

    refactor(select): use local variants for styling

--- packages/core/src/components/select/select-item-indicator.vue
+++ packages/core/src/components/select/select-item-indicator.vue
@@ -1,6 +1,38 @@
 <script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
+import { SelectItemIndicator, useForwardProps } from 'reka-ui'
+import {
+  type SelectItemIndicatorProps,
+  injectCentouiSelectRootContext,
+  selectVariants,
+} from '.'
+import { Icon } from '@iconify/vue'
+import config from '#centoui/config'
+import { computed } from 'vue'
+
+const rootContext = injectCentouiSelectRootContext()
+
+const props = defineProps<SelectItemIndicatorProps>()
+const delegatedProps = reactiveOmit(props, 'class')
+const forwardedProps = useForwardProps(delegatedProps)
+
+const classNames = computed(() => {
+  const { itemIndicator } = selectVariants({
+    size: rootContext.size,
+  })
+  
+  return itemIndicator({ class: props.class })
+})
 </script>
 
 <template>
-  <div />
+  <SelectItemIndicator
+    data-slot="select-item-indicator"
+    v-bind="forwardedProps"
+    :class="classNames"
+  >
+    <slot>
+      <Icon :icon="config.icons.check" />
+    </slot>
+  </SelectItemIndicator>
 </template>
