014ccf14b77fd1981475d3691dfbe0f90e456cb4
Favour Emeka <favorodera@gmail.com>
2026-05-18 19:39:27 +0100

    refactor(popover): use local variants for styling

--- packages/core/src/components/popover/popover-body.vue
+++ packages/core/src/components/popover/popover-body.vue
@@ -2,27 +2,29 @@
 import { reactiveOmit } from '@vueuse/core'
 import { useForwardProps, Primitive } from 'reka-ui'
 import {
-  type PopoverBodySlots,
   type PopoverBodyProps,
-  injectCentouiPopoverRootContext,
+  popoverVariants,
 } from '.'
-
-const rootContext = injectCentouiPopoverRootContext()
-
-defineSlots<PopoverBodySlots>()
+import { computed } from 'vue'
 
 const props = withDefaults(defineProps<PopoverBodyProps>(), {
   as: 'div',
 })
 const delegatedProps = reactiveOmit(props, 'class')
 const forwardedProps = useForwardProps(delegatedProps)
+
+const classNames = computed(() => {
+  const { body } = popoverVariants()
+  
+  return body({ class: props.class })
+})
 </script>
 
 <template>
   <Primitive
     data-slot="popover-body"
     v-bind="forwardedProps"
-    :class="rootContext.styles.body({ class: props.class })"
+    :class="classNames"
   >
     <slot />
   </Primitive>
