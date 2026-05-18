42aa809c388244a17504f9891d2796b8234f1f16
Favour Emeka <favorodera@gmail.com>
2026-05-18 19:39:16 +0100

    refactor(avatar): use local variants for styling

--- packages/core/src/components/avatar/avatar-fallback.vue
+++ packages/core/src/components/avatar/avatar-fallback.vue
@@ -1,22 +1,33 @@
 <script setup lang="ts">
 import { reactiveOmit } from '@vueuse/core'
 import { AvatarFallback, useForwardProps } from 'reka-ui'
-import { type AvatarFallbackProps, type AvatarFallbackSlots, injectCentouiAvatarRootContext } from '.'
+import {
+  type AvatarFallbackProps,
+  avatarVariants,
+  injectCentouiAvatarRootContext,
+} from '.'
+import { computed } from 'vue'
 
 const rootContext = injectCentouiAvatarRootContext()
-
-defineSlots<AvatarFallbackSlots>()
 
 const props = defineProps<AvatarFallbackProps>()
 const delegatedProps = reactiveOmit(props, 'class')
 const forwardedProps = useForwardProps(delegatedProps)
+
+const classNames = computed(() => {
+  const { fallback } = avatarVariants({
+    size: rootContext.size,
+  })
+  
+  return fallback({ class: props.class })
+})
 </script>
 
 <template>
   <AvatarFallback
     data-slot="avatar-fallback"
     v-bind="forwardedProps"
-    :class="rootContext.styles.fallback({ class: props.class })"
+    :class="classNames"
   >
     <slot />
   </AvatarFallback>
