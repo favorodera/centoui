e545f37b39d60d3b01c3a6884d56dcf60fcc020a
Favour Emeka <favorodera@gmail.com>
2026-05-18 19:39:40 +0100

    refactor(select): use local variants for styling

--- packages/core/src/components/select/select-viewport.vue
+++ packages/core/src/components/select/select-viewport.vue
@@ -3,24 +3,31 @@ import { reactiveOmit } from '@vueuse/core'
 import { SelectViewport, useForwardProps } from 'reka-ui'
 import {
   type SelectViewportProps,
-  type SelectViewportSlots,
   injectCentouiSelectRootContext,
+  selectVariants,
 } from '.'
+import { computed } from 'vue'
 
 const rootContext = injectCentouiSelectRootContext()
-
-defineSlots<SelectViewportSlots>()
 
 const props = defineProps<SelectViewportProps>()
 const delegatedProps = reactiveOmit(props, 'class')
 const forwardedProps = useForwardProps(delegatedProps)
+
+const classNames = computed(() => {
+  const { viewport } = selectVariants({
+    size: rootContext.size,
+  })
+  
+  return viewport({ class: props.class })
+})
 </script>
 
 <template>
   <SelectViewport
     v-bind="forwardedProps"
     data-slot="select-viewport"
-    :class="rootContext.styles.viewport({ class: props.class })"
+    :class="classNames"
   >
     <slot />
   </SelectViewport>
