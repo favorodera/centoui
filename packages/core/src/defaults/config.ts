4b7052c3db740c8d22a62da0b4c7b2082e9822ec
Favour Emeka <favorodera@gmail.com>
2026-05-18 19:39:46 +0100

    refactor(config): update default configuration

--- packages/core/src/defaults/config.ts
+++ packages/core/src/defaults/config.ts
@@ -3,6 +3,7 @@ import type { CentoUIConfig } from 'centoui-cli'
 export default {
   icons: {
     chevronDown: 'lucide:chevron-down',
+    chevronUp: 'lucide:chevron-up',
     close: 'lucide:x',
     check: 'lucide:check',
   },
