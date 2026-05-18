211754976ce63ff5b3f8048c0c659cc9fc940561
Favour Emeka <favorodera@gmail.com>
2026-05-18 19:39:38 +0100

    refactor(select): use local variants for styling

--- packages/core/src/components/select/select-scroll-up-button.vue
+++ packages/core/src/components/select/select-scroll-up-button.vue
@@ -1,6 +1,38 @@
 <script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
+import { useForwardProps, SelectScrollUpButton } from 'reka-ui'
+import {
+  injectCentouiSelectRootContext,
+  selectVariants,
+  type SelectScrollUpButtonProps,
+} from '.'
+import { Icon } from '@iconify/vue'
+import config from '#centoui/config'
+import { computed } from 'vue'
+
+const rootContext = injectCentouiSelectRootContext()
+
+const props = defineProps<SelectScrollUpButtonProps>()
+const delegatedProps = reactiveOmit(props, 'class')
+const forwardedProps = useForwardProps(delegatedProps)
+
+const classNames = computed(() => {
+  const { scrollUpButton } = selectVariants({
+    size: rootContext.size,
+  })
+  
+  return scrollUpButton({ class: props.class })
+})
 </script>
 
 <template>
-  <div />
+  <SelectScrollUpButton
+    data-slot="select-scroll-up-button"
+    v-bind="forwardedProps"
+    :class="classNames"
+  >
+    <slot>
+      <Icon :icon="config.icons.chevronUp" />
+    </slot>
+  </SelectScrollUpButton>
 </template>
