90c7e0a87ccd6ba5887987ba63b61fe8c85633a4
Favour Emeka <favorodera@gmail.com>
2026-05-18 19:39:38 +0100

    refactor(select): use local variants for styling

--- packages/core/src/components/select/select-scroll-down-button.vue
+++ packages/core/src/components/select/select-scroll-down-button.vue
@@ -1,6 +1,38 @@
 <script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
+import { useForwardProps, SelectScrollDownButton } from 'reka-ui'
+import { Icon } from '@iconify/vue'
+import config from '#centoui/config'
+import {
+  selectVariants,
+  type SelectScrollDownButtonProps,
+  injectCentouiSelectRootContext,
+} from '.'
+import { computed } from 'vue'
+
+const rootContext = injectCentouiSelectRootContext()
+
+const props = defineProps<SelectScrollDownButtonProps>()
+const delegatedProps = reactiveOmit(props, 'class')
+const forwardedProps = useForwardProps(delegatedProps)
+
+const classNames = computed(() => {
+  const { scrollDownButton } = selectVariants({
+    size: rootContext.size,
+  })
+  
+  return scrollDownButton({ class: props.class })
+})
 </script>
 
 <template>
-  <div />
+  <SelectScrollDownButton
+    data-slot="select-scroll-down-button"
+    v-bind="forwardedProps"
+    :class="classNames"
+  >
+    <slot>
+      <Icon :icon="config.icons.chevronDown" />
+    </slot>
+  </SelectScrollDownButton>
 </template>
