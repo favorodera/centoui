5ad71b8c118b9e6408f57b8b09fedeb6bd9820fe
Favour Emeka <favorodera@gmail.com>
2026-05-18 19:39:28 +0100

    refactor(popover): use local variants for styling

--- packages/core/src/components/popover/popover-content.vue
+++ packages/core/src/components/popover/popover-content.vue
@@ -4,13 +4,9 @@ import { PopoverContent, useForwardPropsEmits } from 'reka-ui'
 import {
   type PopoverContentEmits,
   type PopoverContentProps,
-  type PopoverContentSlots,
-  injectCentouiPopoverRootContext,
+  popoverVariants,
 } from '.'
-
-const rootContext = injectCentouiPopoverRootContext()
-
-defineSlots<PopoverContentSlots>()
+import { computed } from 'vue'
 
 const emits = defineEmits<PopoverContentEmits>()
 
@@ -18,13 +14,19 @@ const props = defineProps<PopoverContentProps>()
 const delegatedProps = reactiveOmit(props, 'class')
 
 const forwardedPropsEmits = useForwardPropsEmits(delegatedProps, emits)
+
+const classNames = computed(() => {
+  const { content } = popoverVariants()
+  
+  return content({ class: props.class })
+})
 </script>
 
 <template>
   <PopoverContent
     data-slot="popover-content"
     v-bind="forwardedPropsEmits"
-    :class="rootContext.styles.content({ class: props.class })"
+    :class="classNames"
   >
     <slot />
   </PopoverContent>
