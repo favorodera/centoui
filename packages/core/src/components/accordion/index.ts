d996ff0389fd7b51ee88822c893e54b5bcd94e59
Favour Emeka <favorodera@gmail.com>
2026-05-18 19:39:05 +0100

    refactor(accordion): update types to use ClassProp and remove styles from context

--- packages/core/src/components/accordion/index.ts
+++ packages/core/src/components/accordion/index.ts
@@ -1,5 +1,4 @@
 import {
-  createContext,
   type AcceptableValue,
   type AccordionContentProps as RekaAccordionContentProps,
   type AccordionHeaderProps as RekaAccordionHeaderProps,
@@ -8,7 +7,7 @@ import {
   type AccordionRootProps as RekaAccordionRootProps,
   type AccordionTriggerProps as RekaAccordionTriggerProps,
 } from 'reka-ui'
-import { tv, type ClassValue, type VariantProps } from 'tailwind-variants'
+import { tv, type ClassProp } from 'tailwind-variants'
 
 export const accordionVariants = tv({
   slots: {
@@ -19,15 +18,15 @@ export const accordionVariants = tv({
     `,
     header: 'flex flex-wrap',
     trigger: `
-      flex flex-1 cursor-pointer items-start justify-between gap-4 py-4
-      font-medium outline-none
+      group/trigger flex flex-1 cursor-pointer items-start justify-between gap-4
+      py-4 font-medium outline-none
       focus-visible:ring-3 focus-visible:ring-ring
-      data-disabled:pointer-events-none data-disabled:opacity-50
-      [&[data-state=open]>[data-slot="accordion-trigger-icon"]]:rotate-180
+      data-disabled:pointer-events-none data-disabled:opacity-75
     `,
     triggerIcon: `
       pointer-events-none h-lh w-4 shrink-0 text-muted-foreground
       transition-transform
+      group-data-[state=open]/trigger:rotate-180
     `,
     content: `
       overflow-hidden
@@ -48,49 +47,17 @@ export { default as AccordionRoot } from './accordion-root.vue'
 export { default as AccordionTrigger } from './accordion-trigger.vue'
 
 
-// CONTEXT
-
-export const [injectCentouiAccordionRootContext, provideCentouiAccordionRootContext] = createContext<AccordionRootContext>('AccordionRoot', 'centoui:accordion-root:context')
-
-
-// TYPES — Variants
-
-export type AccordionVariants = VariantProps<typeof accordionVariants>
-
-
-// TYPES — Context
-export type AccordionRootContext = {
-  /** Styles for the alert */
-  styles: ReturnType<typeof accordionVariants>
-}
-
-
 // TYPES — Props
 
-export type AccordionRootProps = RekaAccordionRootProps & {
-  /** Custom class to apply to the root element. */
-  class?: ClassValue
-}
+export type AccordionRootProps = RekaAccordionRootProps & Pick<ClassProp, 'class'>
 
-export type AccordionItemProps = RekaAccordionItemProps & {
-  /** Custom class to apply to the item element. */
-  class?: ClassValue
-}
+export type AccordionItemProps = RekaAccordionItemProps & Pick<ClassProp, 'class'>
 
-export type AccordionHeaderProps = RekaAccordionHeaderProps & {
-  /** Custom class to apply to the header element. */
-  class?: ClassValue
-}
+export type AccordionHeaderProps = RekaAccordionHeaderProps & Pick<ClassProp, 'class'>
 
-export type AccordionTriggerProps = RekaAccordionTriggerProps & {
-  /** Custom class to apply to the trigger element. */
-  class?: ClassValue
-}
+export type AccordionTriggerProps = RekaAccordionTriggerProps & Pick<ClassProp, 'class'>
 
-export type AccordionContentProps = RekaAccordionContentProps & {
-  /** Custom class to apply to the content element. */
-  class?: ClassValue
-}
+export type AccordionContentProps = RekaAccordionContentProps & Pick<ClassProp, 'class'>
 
 
 // TYPES — Emits
@@ -98,6 +65,7 @@ export type AccordionRootEmits = RekaAccordionRootEmits
 
 
 // TYPES — Slots
+
 export type AccordionRootSlots = {
   default: (props: {
     /** Current active value of the accordion. */
@@ -112,19 +80,8 @@ export type AccordionItemSlots = {
   }) => []
 }
 
-export type AccordionHeaderSlots = {
-  /** Default slot. */
-  default: []
-}
-
 export type AccordionTriggerSlots = {
-  /** Default slot. */
   default: []
-  /** Icon slot. */
   icon: []
-}
-
-export type AccordionContentSlots = {
-  default: []
 }
 
