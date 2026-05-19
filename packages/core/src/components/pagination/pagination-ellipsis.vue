4e858e6462aecdfa3f6b2409189966cff358f890
Favour Emeka <favorodera@gmail.com>
2026-05-19 10:50:35 +0100

    feat(pagination): add pagination ellipsis component
    - implement pagination-ellipsis component
    - integrate reka-ui pagination ellipsis
    - add class variant support

---
+++ packages/core/src/components/pagination/pagination-ellipsis.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { PaginationEllipsis, useForwardProps } from 'reka-ui'
+import {
+  type PaginationEllipsisProps,
+  paginationVariants,
+} from '.'
+import { computed } from 'vue'
+import { reactiveOmit } from '@vueuse/core'
+
+const props = defineProps<PaginationEllipsisProps>()
+const delegatedProps = reactiveOmit(props, 'class')
+const forwardedProps = useForwardProps(delegatedProps)
+
+const classNames = computed(() => {
+  const { ellipsis } = paginationVariants()
+
+  return ellipsis({ class: props.class })
+})
+</script>
+
+<template>
+  <PaginationEllipsis
+    data-slot="pagination-ellipsis"
+    v-bind="forwardedProps"
+    :class="classNames"
+  >
+    <slot />
+  </PaginationEllipsis>
+</template>
