019d7f6dc88c05054d6767c1ec20934a9506f98f
Favour Emeka <favorodera@gmail.com>
2026-05-19 10:51:05 +0100

    feat(pagination): add PaginationLast component

---
+++ packages/core/src/components/pagination/pagination-last.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { PaginationLast, useForwardProps } from 'reka-ui'
+import {
+  type PaginationLastProps,
+  paginationVariants,
+} from '.'
+import { computed } from 'vue'
+import { reactiveOmit } from '@vueuse/core'
+
+const props = defineProps<PaginationLastProps>()
+const delegatedProps = reactiveOmit(props, 'class')
+const forwardedProps = useForwardProps(delegatedProps)
+
+const classNames = computed(() => {
+  const { last } = paginationVariants()
+
+  return last({ class: props.class })
+})
+</script>
+
+<template>
+  <PaginationLast
+    data-slot="pagination-last"
+    v-bind="forwardedProps"
+    :class="classNames"
+  >
+    <slot />
+  </PaginationLast>
+</template>
