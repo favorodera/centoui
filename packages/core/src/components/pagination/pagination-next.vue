948cf23671999596cc5a6a855a98e352c1f913f4
Favour Emeka <favorodera@gmail.com>
2026-05-19 22:58:54 +0100

    feat(pagination): enhance pagination-next component
    - Add variant, size, and square props support
    - Inherit styles from root pagination context
    - Provide default next icon and sr-only text

--- packages/core/src/components/pagination/pagination-next.vue
+++ packages/core/src/components/pagination/pagination-next.vue
@@ -1,20 +1,40 @@
 <script setup lang="ts">
 import { PaginationNext, useForwardProps } from 'reka-ui'
+import { computed } from 'vue'
+import { reactiveOmit } from '@vueuse/core'
+import { Icon } from '@iconify/vue'
+import config from '#centoui/config'
+import { buttonVariants } from '../button'
 import {
   type PaginationNextProps,
   paginationVariants,
+  injectCentouiPaginationRootContext,
 } from '.'
-import { computed } from 'vue'
-import { reactiveOmit } from '@vueuse/core'
 
-const props = defineProps<PaginationNextProps>()
-const delegatedProps = reactiveOmit(props, 'class')
+const rootContext = injectCentouiPaginationRootContext()
+
+const props = withDefaults(defineProps<PaginationNextProps>(), {
+  square: undefined,
+})
+const delegatedProps = reactiveOmit(props, 'class', 'size', 'variant', 'square')
 const forwardedProps = useForwardProps(delegatedProps)
 
-const classNames = computed(() => {
-  const { next } = paginationVariants()
+const resolvedVariant = computed(() => props.variant ?? rootContext.variant)
+const resolvedSize = computed(() => props.size ?? rootContext.size)
+const resolvedSquare = computed(() => props.square ?? rootContext.square)
 
-  return next({ class: props.class })
+const classNames = computed(() => {
+  const { root: buttonRoot } = buttonVariants({
+    variant: resolvedVariant.value,
+    size: resolvedSize.value,
+    square: resolvedSquare.value,
+  })
+
+  const { next: paginationNext } = paginationVariants()
+
+  return buttonRoot({
+    class: paginationNext({ class: props.class }),
+  })
 })
 </script>
 
@@ -22,8 +42,13 @@ const classNames = computed(() => {
   <PaginationNext
     data-slot="pagination-next"
     v-bind="forwardedProps"
+    :data-variant="resolvedVariant"
+    :data-size="resolvedSize"
     :class="classNames"
   >
-    <slot />
+    <slot>
+      <Icon :icon="config.icons.chevronRight" />
+      <span class="sr-only">Next page</span>
+    </slot>
   </PaginationNext>
 </template>
