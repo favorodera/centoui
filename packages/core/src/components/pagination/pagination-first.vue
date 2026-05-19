4d5a12ad4b25a841bfe85ae883975ecc8f5408c6
Favour Emeka <favorodera@gmail.com>
2026-05-19 10:50:50 +0100

    feat(pagination): add first page button component

---
+++ packages/core/src/components/pagination/pagination-first.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { PaginationFirst, useForwardProps } from 'reka-ui'
+import {
+  type PaginationFirstProps,
+  paginationVariants,
+} from '.'
+import { computed } from 'vue'
+import { reactiveOmit } from '@vueuse/core'
+
+const props = defineProps<PaginationFirstProps>()
+const delegatedProps = reactiveOmit(props, 'class')
+const forwardedProps = useForwardProps(delegatedProps)
+
+const classNames = computed(() => {
+  const { first } = paginationVariants()
+
+  return first({ class: props.class })
+})
+</script>
+
+<template>
+  <PaginationFirst
+    data-slot="pagination-first"
+    v-bind="forwardedProps"
+    :class="classNames"
+  >
+    <slot />
+  </PaginationFirst>
+</template>
