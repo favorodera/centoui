2e4470132963a395d36105b5ec80f936b168a759
Favour Emeka <favorodera@gmail.com>
2026-05-19 10:53:49 +0100

    feat(pagination): add PaginationNext component wrapper

---
+++ packages/core/src/components/pagination/pagination-next.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { PaginationNext, useForwardProps } from 'reka-ui'
+import {
+  type PaginationNextProps,
+  paginationVariants,
+} from '.'
+import { computed } from 'vue'
+import { reactiveOmit } from '@vueuse/core'
+
+const props = defineProps<PaginationNextProps>()
+const delegatedProps = reactiveOmit(props, 'class')
+const forwardedProps = useForwardProps(delegatedProps)
+
+const classNames = computed(() => {
+  const { next } = paginationVariants()
+
+  return next({ class: props.class })
+})
+</script>
+
+<template>
+  <PaginationNext
+    data-slot="pagination-next"
+    v-bind="forwardedProps"
+    :class="classNames"
+  >
+    <slot />
+  </PaginationNext>
+</template>
