033da802673576fdba98b2db66e457caa0dbce2e
Favour Emeka <favorodera@gmail.com>
2026-05-19 10:54:32 +0100

    feat(pagination): add prev button component

---
+++ packages/core/src/components/pagination/pagination-prev.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { PaginationPrev, useForwardProps } from 'reka-ui'
+import {
+  type PaginationPrevProps,
+  paginationVariants,
+} from '.'
+import { computed } from 'vue'
+import { reactiveOmit } from '@vueuse/core'
+
+const props = defineProps<PaginationPrevProps>()
+const delegatedProps = reactiveOmit(props, 'class')
+const forwardedProps = useForwardProps(delegatedProps)
+
+const classNames = computed(() => {
+  const { prev } = paginationVariants()
+
+  return prev({ class: props.class })
+})
+</script>
+
+<template>
+  <PaginationPrev
+    data-slot="pagination-prev"
+    v-bind="forwardedProps"
+    :class="classNames"
+  >
+    <slot />
+  </PaginationPrev>
+</template>
