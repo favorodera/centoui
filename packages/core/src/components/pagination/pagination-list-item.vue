f1cf811ee094e96e7ed6c7ea581a0c3964d90fad
Favour Emeka <favorodera@gmail.com>
2026-05-19 22:58:36 +0100

    feat(pagination): resolve variant and size
    - Inject pagination root context
    - Resolve variant and size dynamically
    - Apply button variants to list item
    - Omit non-delegated props correctly

--- packages/core/src/components/pagination/pagination-list-item.vue
+++ packages/core/src/components/pagination/pagination-list-item.vue
@@ -2,25 +2,55 @@
 import { PaginationListItem, useForwardProps } from 'reka-ui'
 import {
   type PaginationListItemProps,
+  injectCentouiPaginationRootContext,
   paginationVariants,
 } from '.'
 import { computed } from 'vue'
 import { reactiveOmit } from '@vueuse/core'
+import { buttonVariants } from '../button'
 
-const props = defineProps<PaginationListItemProps>()
-const delegatedProps = reactiveOmit(props, 'class')
+const rootContext = injectCentouiPaginationRootContext()
+
+const props = withDefaults(defineProps<PaginationListItemProps>(), {
+  active: false,
+})
+const delegatedProps = reactiveOmit(
+  props,
+  'class',
+  'activeVariant',
+  'variant',
+  'size',
+  'active',
+)
 const forwardedProps = useForwardProps(delegatedProps)
 
-const classNames = computed(() => {
-  const { listItem } = paginationVariants()
+const resolvedVariant = computed(() => {
+  const activeVariant = props.activeVariant ?? rootContext.activeVariant
+  const variant = props.variant ?? rootContext.variant
 
-  return listItem({ class: props.class })
+  return props.active ? activeVariant : variant
+})
+const resolvedSize = computed(() => props.size ?? rootContext.size)
+
+const classNames = computed(() => {
+  const { root: buttonRoot } = buttonVariants({
+    variant: resolvedVariant.value,
+    size: resolvedSize.value,
+  })
+
+  const { listItem: paginationListItem } = paginationVariants()
+
+  return buttonRoot({
+    class: paginationListItem({ class: props.class }),
+  })
 })
 </script>
 
 <template>
   <PaginationListItem
     data-slot="pagination-list-item"
+    :data-size="resolvedSize"
+    :data-variant="resolvedVariant"
     v-bind="forwardedProps"
     :class="classNames"
   >
