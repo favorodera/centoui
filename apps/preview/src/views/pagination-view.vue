9b6de2789317917df2c28e0d0e2cbbb94b4f110f
Favour Emeka <favorodera@gmail.com>
2026-05-19 10:49:03 +0100

    feat(pagination): add preview for pagination component

---
+++ apps/preview/src/views/pagination-view.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import {
+  PaginationRoot,
+  PaginationList,
+  PaginationListItem,
+  PaginationEllipsis,
+  PaginationFirst,
+  PaginationPrev,
+  PaginationNext,
+  PaginationLast,
+} from '#centoui/components/pagination'
+import ViewContainer from '@/components/view-container.vue'
+import { usePreview } from '@/composables/use-preview'
+
+usePreview('Pagination', {})
+</script>
+
+<template>
+  <ViewContainer>
+    <PaginationRoot
+      :total="100"
+      :sibling-count="1"
+      :items-per-page="10"
+      show-edges
+      :default-page="2"
+    >
+      <PaginationList
+        v-slot="{ items }"
+      >
+        <PaginationFirst />
+
+        <PaginationPrev />
+
+        <template v-for="(page, index) in items">
+          <PaginationListItem
+            v-if="page.type === 'page'"
+            :key="index"
+            :value="page.value"
+          >
+            {{ page.value }}
+          </PaginationListItem>
+
+          <PaginationEllipsis
+            v-else
+            :key="page.type"
+            :index="index"
+          />
+        </template>
+
+        <PaginationNext />
+
+        <PaginationLast />
+      </PaginationList>
+    </PaginationRoot>
+  </ViewContainer>
+</template>
