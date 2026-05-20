50d610daffcf58772095b3132cbdc8f9f9664d41
Favour Emeka <favorodera@gmail.com>
2026-05-20 18:44:24 +0100

    feat(preview): add Switch component preview view

---
+++ apps/preview/src/views/switch-view.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { SwitchRoot, SwitchThumb, switchVariants } from '#centoui/components/switch'
+import ViewContainer from '@/components/view-container.vue'
+import { usePreview } from '@/composables/use-preview'
+import { ref } from 'vue'
+
+const values = usePreview('Switch', {
+  size: {
+    type: 'select',
+    label: 'Size',
+    options: Object.keys(switchVariants.variants.size),
+    default: switchVariants.defaultVariants.size,
+  },
+})
+
+const switchState = ref(false)
+</script>
+
+<template>
+  <ViewContainer>
+    <SwitchRoot
+      v-model="switchState"
+      :size="values.size"
+    >
+      <SwitchThumb />
+    </SwitchRoot>
+  </ViewContainer>
+</template>
+
