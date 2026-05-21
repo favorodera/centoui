bc19204c622fb4f2ecdf55771610412538147ec8
Favour Emeka <favorodera@gmail.com>
2026-05-21 18:28:00 +0100

    feat(App): replace ComponentPreview with App component in main template

--- apps/preview/src/App.vue
+++ apps/preview/src/App.vue
@@ -1,19 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import ComponentPreview from './components/component-preview.vue'
+import App from '@/components/app.vue'
 </script>
 
 <template>
-  <ComponentPreview>
+  <App>
     <RouterView />
-  </ComponentPreview>
+  </App>
 </template>
-
-<style>
-html, body, #app {
-  height: 100%;
-  margin: 0;
-  padding: 0;
-}
-</style>
-
