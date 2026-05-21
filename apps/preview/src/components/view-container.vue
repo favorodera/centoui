5f2785d53c9de8b3335dec1cbb788df703be7ea3
Favour Emeka <favorodera@gmail.com>
2026-05-21 21:46:30 +0100

    style(view-container): adjust max-width for improved layout consistency

--- apps/preview/src/components/view-container.vue
+++ apps/preview/src/components/view-container.vue
@@ -2,9 +2,10 @@
   <div
     class="
       scrollbar-thumb-rounded flex
-      max-h-[calc(100dvh-(--spacing(11))-(--spacing(3)))] w-full scrollbar-thin
-      scrollbar-thumb-border scrollbar-track-transparent flex-wrap items-center
-      justify-center gap-8 overflow-auto bg-background p-3
+      max-h-[calc(100dvh-(--spacing(11))-(--spacing(3)))] w-full max-w-3xl
+      scrollbar-thin scrollbar-thumb-border scrollbar-track-transparent
+      flex-wrap items-center justify-center gap-8 overflow-auto bg-background
+      p-3
     "
   >
     <slot />
