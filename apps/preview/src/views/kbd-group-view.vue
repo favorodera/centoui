f051299b23ce67393ee93405db144ee56a116d2b
Favour Emeka <favorodera@gmail.com>
2026-05-21 22:05:56 +0100

    style(kbd-group-view): simplify keyboard size handling by removing unused size props

--- apps/preview/src/views/kbd-group-view.vue
+++ apps/preview/src/views/kbd-group-view.vue
@@ -1,44 +1,29 @@
 <script setup lang="ts">
-import { Kbd, kbdVariants } from '#centoui/components/kbd'
+import { Kbd } from '#centoui/components/kbd'
 import { KbdGroup } from '#centoui/components/kbd-group'
 import ViewContainer from '@/components/view-container.vue'
 import { useApp } from '@/composables/use-app'
 
-const values = useApp().preview.initPreview('Kbd Group', {
-  kbdSize: {
-    type: 'array',
-    label: 'Kbd Size',
-    options: Object.keys(kbdVariants.variants.size),
-    default: kbdVariants.defaultVariants.size,
-  },
-})
+useApp().preview.initPreview('Kbd Group', {})
 </script>
 
 <template>
   <ViewContainer>
     <KbdGroup>
-      <Kbd
-        :size="values.kbdSize"
-      >
+      <Kbd>
         ⌘
       </Kbd>
-      <Kbd
-        :size="values.kbdSize"
-      >
+      <Kbd>
         K
       </Kbd>
     </KbdGroup>
 
     <KbdGroup>
-      <Kbd
-        :size="values.kbdSize"
-      >
+      <Kbd>
         Ctrl
       </Kbd>
 
-      <Kbd
-        :size="values.kbdSize"
-      >
+      <Kbd>
         C
       </Kbd>
     </KbdGroup>
