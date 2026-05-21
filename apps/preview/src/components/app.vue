6423668677596bf6de8730cae4bff8c92d606059
Favour Emeka <favorodera@gmail.com>
2026-05-21 18:32:09 +0100

    feat(app): create app component with header and body structure

---
+++ apps/preview/src/components/app.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Header from '@/components/header.vue'
+import Body from '@/components/body.vue'
+</script>
+
+<template>
+  <div class="h-dvh grid grid-rows-[auto_1fr] grid-cols-1 place-items-center w-full">
+    <Header />
+
+    <Body>
+      <slot />
+    </Body>
+  </div>
+</template>
