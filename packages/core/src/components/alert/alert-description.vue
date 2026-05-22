2b89ae6b2f8a6a7c8b81bb466a222fcd86ad4ed0
Favour Emeka <favorodera@gmail.com>
2026-05-22 18:04:14 +0100

    refactor(alert): remove default as prop
    - Remove withDefaults from alert description

--- packages/core/src/components/alert/alert-description.vue
+++ packages/core/src/components/alert/alert-description.vue
@@ -6,9 +6,7 @@ import { computed } from 'vue'
 
 const rootContext = injectCentouiAlertRootContext()
 
-const props = withDefaults(defineProps<AlertDescriptionProps>(), {
-  as: 'div',
-})
+const props = defineProps<AlertDescriptionProps>()
 const delegatedProps = reactiveOmit(props, 'class')
 const forwardedProps = useForwardProps(delegatedProps)
 
