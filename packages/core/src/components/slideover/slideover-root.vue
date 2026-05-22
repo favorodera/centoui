a7eb6c771ac805d8ad1913176f59fc2845b31ade
Favour Emeka <favorodera@gmail.com>
2026-05-22 23:27:55 +0100

    feat(slideover): add SlideoverRoot component

---
+++ packages/core/src/components/slideover/slideover-root.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { DialogRoot, useForwardPropsEmits } from 'reka-ui'
+import { type SlideoverRootEmits, type SlideoverRootProps, type SlideoverRootSlots } from '.'
+
+defineSlots<SlideoverRootSlots>()
+
+const emits = defineEmits<SlideoverRootEmits>()
+
+const props = defineProps<SlideoverRootProps>()
+  
+const forwardedPropsEmits = useForwardPropsEmits(props, emits)
+</script>
+
+<template>
+  <DialogRoot
+    v-slot="slotProps"
+    v-bind="forwardedPropsEmits"
+    data-slot="slideover-root"
+  >
+    <slot v-bind="slotProps" />
+  </DialogRoot>
+</template>
