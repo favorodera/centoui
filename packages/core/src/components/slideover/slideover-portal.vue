dc3b6653311b69e7ac2f7dd36550826e99d0462a
Favour Emeka <favorodera@gmail.com>
2026-05-22 23:27:37 +0100

    feat(slideover): add SlideoverPortal component

---
+++ packages/core/src/components/slideover/slideover-portal.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useForwardProps, DialogPortal } from 'reka-ui'
+import {
+  type SlideoverPortalProps,
+} from '.'
+
+const props = defineProps<SlideoverPortalProps>()
+const forwardedProps = useForwardProps(props)
+</script>
+
+<template>
+  <DialogPortal
+    v-bind="forwardedProps"
+  >
+    <slot />
+  </DialogPortal>
+</template>
