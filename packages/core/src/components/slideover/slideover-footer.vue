1506700096bdb7e683d176b97cf6a00da3096332
Favour Emeka <favorodera@gmail.com>
2026-05-22 23:13:51 +0100

    feat(slideover): add SlideoverFooter component - Add new component for slideover footer. - Integrates with slideover variants. - Utilizes reka-ui Primitive.

---
+++ packages/core/src/components/slideover/slideover-footer.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
+import { useForwardProps, Primitive } from 'reka-ui'
+import {
+  type SlideoverFooterProps,
+  slideoverVariants,
+} from '.'
+import { computed } from 'vue'
+
+const props = defineProps<SlideoverFooterProps>()
+const delegatedProps = reactiveOmit(props, 'class')
+const forwardedProps = useForwardProps(delegatedProps)
+
+const classNames = computed(() => {
+  const { footer } = slideoverVariants()
+  
+  return footer({ class: props.class })
+})
+</script>
+
+<template>
+  <Primitive
+    data-slot="slideover-footer"
+    v-bind="forwardedProps"
+    :class="classNames"
+  >
+    <slot />
+  </Primitive>
+</template>
