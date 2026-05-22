c2aa348530e83589020cbe84829b994e4f3ded1c
Favour Emeka <favorodera@gmail.com>
2026-05-22 23:30:17 +0100

    feat(slideover): add SlideoverTitle component

---
+++ packages/core/src/components/slideover/slideover-title.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
+import { useForwardProps, DialogTitle } from 'reka-ui'
+import {
+  type SlideoverTitleProps,
+  slideoverVariants,
+} from '.'
+import { computed } from 'vue'
+
+const props = defineProps<SlideoverTitleProps>()
+const delegatedProps = reactiveOmit(props, 'class')
+const forwardedProps = useForwardProps(delegatedProps)
+
+const classNames = computed(() => {
+  const { title } = slideoverVariants()
+  
+  return title({ class: props.class })
+})
+</script>
+
+<template>
+  <DialogTitle
+    data-slot="slideover-title"
+    v-bind="forwardedProps"
+    :class="classNames"
+  >
+    <slot />
+  </DialogTitle>
+</template>
