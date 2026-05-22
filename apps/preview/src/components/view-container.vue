772a15b9f98ae24cef9fad0e9c961411c7b6a216
Favour Emeka <favorodera@gmail.com>
2026-05-22 18:00:52 +0100

    style(preview): use size-full in view container

--- apps/preview/src/components/view-container.vue
+++ apps/preview/src/components/view-container.vue
@@ -2,7 +2,7 @@
   <div
     class="
       scrollbar-thumb-rounded flex
-      max-h-[calc(100dvh-(--spacing(11))-(--spacing(3)))] w-full max-w-3xl
+      max-h-[calc(100dvh-(--spacing(11))-(--spacing(3)))] size-full max-w-3xl
       scrollbar-thin scrollbar-thumb-border scrollbar-track-transparent
       flex-wrap items-center justify-center gap-8 overflow-auto bg-background
       p-3
