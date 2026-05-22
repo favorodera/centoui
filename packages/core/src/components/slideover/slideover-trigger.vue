4ed708446b80849fd9ad6612de1c4662f28d1ead
Favour Emeka <favorodera@gmail.com>
2026-05-22 23:31:04 +0100

    feat(slideover): add SlideoverTrigger component

---
+++ packages/core/src/components/slideover/slideover-trigger.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
+import { useForwardProps, DialogTrigger } from 'reka-ui'
+import {
+  type SlideoverTriggerProps,
+  slideoverVariants,
+} from '.'
+import { computed } from 'vue'
+
+const props = defineProps<SlideoverTriggerProps>()
+const delegatedProps = reactiveOmit(props, 'class')
+const forwardedProps = useForwardProps(delegatedProps)
+
+const classNames = computed(() => {
+  const { trigger } = slideoverVariants()
+  
+  return trigger({ class: props.class })
+})
+</script>
+
+<template>
+  <DialogTrigger
+    data-slot="slideover-trigger"
+    v-bind="forwardedProps"
+    :class="classNames"
+  >
+    <slot />
+  </DialogTrigger>
+</template>
