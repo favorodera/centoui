744eed370de843654ed2d6a187e41cad92b9983c
Favour Emeka <favorodera@gmail.com>
2026-05-22 18:02:11 +0100

    style(preview): reorder view-container classes

--- apps/preview/src/components/view-container.vue
+++ apps/preview/src/components/view-container.vue
@@ -1,8 +1,8 @@
 <template>
   <div
     class="
-      scrollbar-thumb-rounded flex
-      max-h-[calc(100dvh-(--spacing(11))-(--spacing(3)))] size-full max-w-3xl
+      scrollbar-thumb-rounded flex size-full
+      max-h-[calc(100dvh-(--spacing(11))-(--spacing(3)))] max-w-3xl
       scrollbar-thin scrollbar-thumb-border scrollbar-track-transparent
       flex-wrap items-center justify-center gap-8 overflow-auto bg-background
       p-3
