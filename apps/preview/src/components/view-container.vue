866d66ed8ef2d5fd0c78a01aa4dd3a3de593e6cb
Favour Emeka <favorodera@gmail.com>
2026-05-23 07:15:04 +0100

    style(preview): remove scrollbar-thumb-rounded

--- apps/preview/src/components/view-container.vue
+++ apps/preview/src/components/view-container.vue
@@ -1,11 +1,10 @@
 <template>
   <div
     class="
-      scrollbar-thumb-rounded flex size-full
-      max-h-[calc(100dvh-(--spacing(11))-(--spacing(3)))] max-w-3xl
-      scrollbar-thin scrollbar-thumb-border scrollbar-track-transparent
-      flex-wrap items-center justify-center gap-8 overflow-auto bg-background
-      p-3
+      flex size-full max-h-[calc(100dvh-(--spacing(11))-(--spacing(3)))]
+      max-w-3xl scrollbar-thin scrollbar-thumb-border
+      scrollbar-track-transparent flex-wrap items-center justify-center gap-8
+      overflow-auto bg-background p-3
     "
   >
     <slot />
