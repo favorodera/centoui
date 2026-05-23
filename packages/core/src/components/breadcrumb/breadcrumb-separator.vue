f33e1b3b8aa4e8ce56f23ee4791a86a8b79b682d
Favour Emeka <favorodera@gmail.com>
2026-05-23 20:22:11 +0100

    feat(breadcrumb): add separator component

---
+++ packages/core/src/components/breadcrumb/breadcrumb-separator.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
+import { Primitive, useForwardProps } from 'reka-ui'
+import { computed } from 'vue'
+import { breadcrumbVariants, type BreadcrumbSeparatorProps } from '.'
+import { Icon } from '@iconify/vue'
+import config from '#centoui/config'
+
+const props = defineProps<BreadcrumbSeparatorProps>()
+const delegatedProps = reactiveOmit(props, 'class')
+const forwardedProps = useForwardProps(delegatedProps)
+
+const classNames = computed(() => {
+  const { separator } = breadcrumbVariants()
+
+  return separator({ class: props.class })
+})
+</script>
+
+<template>
+  <Primitive
+    data-slot="breadcrumb-separator"
+    v-bind="forwardedProps"
+    :class="classNames"
+    role="presentation"
+    aria-hidden="true"
+  >
+    <slot>
+      <Icon :icon="config.icons.chevronRight" />
+    </slot>
+  </Primitive>
+</template>
