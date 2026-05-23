db89def2f1e73a01a0588dfa0ce866506b6a7a10
Favour Emeka <favorodera@gmail.com>
2026-05-23 20:20:28 +0100

    feat(breadcrumb): add breadcrumb-root component
    - implement breadcrumb-root component
    - use reka-ui primitive and forward props

---
+++ packages/core/src/components/breadcrumb/breadcrumb-root.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
+import { Primitive, useForwardProps } from 'reka-ui'
+import { computed } from 'vue'
+import { breadcrumbVariants, type BreadcrumbRootProps } from '.'
+
+const props = withDefaults(defineProps<BreadcrumbRootProps>(), {
+  as: 'nav',
+})
+const delegatedProps = reactiveOmit(props, 'class')
+const forwardedProps = useForwardProps(delegatedProps)
+
+const classNames = computed(() => {
+  const { root } = breadcrumbVariants()
+
+  return root({ class: props.class })
+})
+</script>
+
+<template>
+  <Primitive
+    data-slot="breadcrumb-root"
+    v-bind="forwardedProps"
+    :class="classNames"
+    aria-label="breadcrumb"
+  >
+    <slot />
+  </Primitive>
+</template>
