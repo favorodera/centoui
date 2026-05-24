96fa6a728273c6e71fab3024bd2b5a30c01f5b94
Favour Emeka <favorodera@gmail.com>
2026-05-24 18:00:33 +0100

    refactor(core): update Icon import to internal path

--- packages/core/src/components/breadcrumb/breadcrumb-ellipsis.vue
+++ packages/core/src/components/breadcrumb/breadcrumb-ellipsis.vue
@@ -3,7 +3,7 @@ import { reactiveOmit } from '@vueuse/core'
 import { Primitive, useForwardProps } from 'reka-ui'
 import { computed } from 'vue'
 import { breadcrumbVariants, type BreadcrumbEllipsisProps } from '.'
-import { Icon } from '@iconify/vue'
+import { Icon } from '../icon'
 import config from '#centoui/config'
 
 const props = defineProps<BreadcrumbEllipsisProps>()
