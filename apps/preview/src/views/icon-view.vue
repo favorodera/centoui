6b89520159386fa7dc543a640588e9061f0d514c
Favour Emeka <favorodera@gmail.com>
2026-05-24 17:39:51 +0100

    feat(preview): add icon preview page
    - Add view to preview Icon component

---
+++ apps/preview/src/views/icon-view.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Icon } from '#centoui/components/icon'
+import ViewContainer from '@/components/view-container.vue'
+import { useApp } from '@/composables/use-app'
+
+useApp().preview.initPreview('Icon', {})
+</script>
+
+<template>
+  <ViewContainer>
+
+    <Icon icon="lucide:plus" />
+
+  </ViewContainer>
+</template>
