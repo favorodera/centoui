28e932841452922d4fa07e85927b51fddeccb24c
Favour Emeka <favorodera@gmail.com>
2026-05-28 12:02:51 +0100

    feat(preview): add label view

---
+++ apps/preview/src/views/label-view.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { SwitchRoot, SwitchThumb } from '#centoui/components/switch'
+import { Label } from '#centoui/components/label'
+import ViewContainer from '@/components/view-container.vue'
+import { useApp } from '@/composables/use-app'
+import { ref } from 'vue'
+
+useApp().preview.initPreview('Label', {})
+
+const switchState = ref(false)
+</script>
+
+<template>
+  <ViewContainer>
+    <Label
+      for="switch"
+    >
+      Click on Switch label
+    </Label>
+
+    <SwitchRoot
+      id="switch"
+      v-model="switchState"
+    >
+      <SwitchThumb />
+    </SwitchRoot>
+  </ViewContainer>
+</template>
+
