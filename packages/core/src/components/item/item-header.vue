de968874fb1bd57b4e26b775effd6be0f48fbccf
Favour Emeka <favorodera@gmail.com>
2026-05-29 22:25:19 +0100

    feat(item): implement ItemHeader component
    - add ItemHeader component
    - export ItemHeader from item entry point

---
+++ packages/core/src/components/item/item-header.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
+import { Primitive, useForwardProps } from 'reka-ui'
+import { computed } from 'vue'
+import { injectCentouiItemRootContext, itemVariants, type ItemHeaderProps } from '.'
+
+const rootContext = injectCentouiItemRootContext()
+
+const props = defineProps<ItemHeaderProps>()
+const delegatedProps = reactiveOmit(props, 'class')
+const forwardedProps = useForwardProps(delegatedProps)
+
+const classNames = computed(() => {
+  const { header } = itemVariants({
+    size: rootContext.size,
+    variant: rootContext.variant,
+    orientation: rootContext.orientation,
+  })
+
+  return header({ class: props.class })
+})
+</script>
+
+<template>
+  <Primitive
+    data-slot="item-header"
+    v-bind="forwardedProps"
+    :class="classNames"
+  >
+    <slot />
+  </Primitive>
+</template>
