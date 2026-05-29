0e7d3d2ccecf0dff84206c274fb932a2b7ae6b7a
Favour Emeka <favorodera@gmail.com>
2026-05-29 22:21:07 +0100

    feat(item): add ItemMedia component

---
+++ packages/core/src/components/item/item-media.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
+import { Primitive, useForwardProps } from 'reka-ui'
+import { computed } from 'vue'
+import { injectCentouiItemRootContext, itemVariants, type ItemMediaProps } from '.'
+
+const rootContext = injectCentouiItemRootContext()
+
+const props = defineProps<ItemMediaProps>()
+const delegatedProps = reactiveOmit(props, 'class')
+const forwardedProps = useForwardProps(delegatedProps)
+
+const classNames = computed(() => {
+  const { media } = itemVariants({
+    size: rootContext.size,
+    variant: rootContext.variant,
+    orientation: rootContext.orientation,
+  })
+
+  return media({ class: props.class })
+})
+</script>
+
+<template>
+  <Primitive
+    data-slot="item-media"
+    v-bind="forwardedProps"
+    :class="classNames"
+  >
+    <slot />
+  </Primitive>
+</template>
