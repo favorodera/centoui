6f7233ce955e0093c8af386b065d86535c5cfe3c
Favour Emeka <favorodera@gmail.com>
2026-05-29 13:22:14 +0100

    style(preview): reorder CSS classes in view container - Standardize utility class order - Replace `size-full` with `w-full` - Improve readability

--- apps/preview/src/components/view-container.vue
+++ apps/preview/src/components/view-container.vue
@@ -1,10 +1,10 @@
 <template>
   <div
     class="
-      flex size-full max-h-[calc(100dvh-(--spacing(11))-(--spacing(3)))]
-      max-w-3xl scrollbar-thin scrollbar-thumb-border
-      scrollbar-track-transparent flex-wrap items-center justify-center gap-8
-      overflow-auto bg-background p-3
+      flex max-h-[calc(100dvh-(--spacing(11))-(--spacing(3)))] w-full max-w-3xl
+      scrollbar-thin scrollbar-thumb-border scrollbar-track-transparent
+      flex-wrap items-center justify-center gap-8 overflow-auto bg-background
+      p-3
     "
   >
     <slot />
