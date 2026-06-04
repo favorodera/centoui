1bd89df45ba580f5050e08222d22ee1921895f38
Favour Emeka <favorodera@gmail.com>
2026-06-04 07:33:59 +0100

    style(progress): reduce root height to 1.5 units

--- packages/core/src/components/progress/index.ts
+++ packages/core/src/components/progress/index.ts
@@ -6,7 +6,7 @@ import { tv } from 'tailwind-variants'
 
 export const progressBarVariants = tv({
   slots: {
-    root: 'relative h-2 w-full overflow-hidden rounded-xs bg-input',
+    root: 'relative h-1.5 w-full overflow-hidden rounded-xs bg-input',
     indicator: `
       size-full origin-left rounded-[inherit] bg-primary transition-all
     `,
