faf3bd860a7ed8589783313c6b4199136d3d99a9
Favour Emeka <favorodera@gmail.com>
2026-06-17 16:38:29 +0100

    feat(storybook): add badge component story
    - create new story for badge component
    - implement dynamic status variants
    - add examples with icons and loading state

---
+++ apps/storybook/src/stories/badge.story.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { Badge, badgeVariants } from '#centoui/components/badge'
+import { Icon } from '#centoui/components/icon'
+import { useStory } from '@/composables/use-story'
+
+const values = useStory('Badge', {
+  status: {
+    default: badgeVariants.defaultVariants.status,
+    options: Object.keys(badgeVariants.variants.status),
+    type: 'array',
+  },
+})
+</script>
+
+<template>
+  <Badge :status="values.status">
+    Badge
+  </Badge>
+
+  <Badge :status="values.status">
+    <Icon icon="lucide:user" />
+    User
+  </Badge>
+
+  <Badge :status="values.status">
+    Loading...
+    <Icon icon="svg-spinners:180-ring" />
+  </Badge>
+</template>
