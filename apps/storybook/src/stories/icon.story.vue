d15b5deb5cba89b150d74ec09a7e98c277a4b2c4
Favour Emeka <favorodera@gmail.com>
2026-06-19 19:04:14 +0100

    feat(storybook): add icon component story
    - create icon.story.vue
    - integrate with useStory composable
    - support icon property customization

---
+++ apps/storybook/src/stories/icon.story.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Icon } from '#centoui/components/icon'
+import { useStory } from '@/composables/use-story'
+
+const values = useStory('Icon', {
+  icon: {
+    default: 'lucide:user',
+    hint: 'Iconify icon IDs',
+    type: 'string',
+  },
+})
+</script>
+
+<template>
+  <Icon :icon="values.icon" />
+</template>
