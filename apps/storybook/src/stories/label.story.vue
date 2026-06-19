54dab2171825de1290ea68fde4a90c1d94f793ce
Favour Emeka <favorodera@gmail.com>
2026-06-19 20:04:57 +0100

    feat(storybook): add label component story
    - create new label story file
    - include switch component integration
    - add storybook controls for disabled state

---
+++ apps/storybook/src/stories/label.story.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { Label } from '#centoui/components/label'
+import { Switch } from '#centoui/components/switch'
+import { useStory } from '@/composables/use-story'
+
+const values = useStory('Input', {
+  disabled: {
+    default: false,
+    type: 'boolean',
+  },
+})
+</script>
+
+<template>
+  <Label class="flex items-center gap-2">
+    Toggle notifications
+    <Switch
+      id="switch"
+      :disabled="values.disabled"
+      name="switch"
+    />
+  </Label>
+</template>
