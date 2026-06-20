292b56144e9bed36d3d49745a34497f76771ec4a
Favour Emeka <favorodera@gmail.com>
2026-06-20 19:06:31 +0100

    feat(storybook): add separator component story

---
+++ apps/storybook/src/stories/separator.story.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Separator, separatorVariants } from '#centoui/components/separator'
+import { useStory } from '@/composables/use-story'
+
+const controls = useStory('Separator', {
+  orientation: {
+    default: separatorVariants.defaultVariants.orientation,
+    options: Object.keys(separatorVariants.variants.orientation),
+    type: 'array',
+  },
+})
+</script>
+
+<template>
+  <Separator :orientation="controls.orientation" />
+
+  <Separator :orientation="controls.orientation">
+    OR
+  </Separator>
+</template>
