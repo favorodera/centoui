2f5244c17b1870f6231c4a35c83ca41b4a04b73b
Favour Emeka <favorodera@gmail.com>
2026-06-20 19:02:21 +0100

    feat(storybook): add select component story
    - add interactive select component story
    - include variant and state controls

---
+++ apps/storybook/src/stories/select.story.vue
@@ -0,0 +1,102 @@
+<script setup lang="ts">
+import { SelectContent, SelectGroup, SelectItem, SelectLabel, SelectRoot, SelectTrigger, SelectValue, selectVariants } from '#centoui/components/select'
+import { useStory } from '@/composables/use-story'
+
+const controls = useStory('Select', {
+  align: {
+    default: 'center' as any,
+    options: [
+      'center',
+      'start',
+      'end',
+    ],
+    type: 'array',
+  },
+  contentPosition: {
+    default: selectVariants.defaultVariants.contentPosition,
+    options: Object.keys(selectVariants.variants.contentPosition),
+    type: 'array',
+  },
+  disabled: {
+    default: false,
+    type: 'boolean',
+  },
+  multiple: {
+    default: false,
+    type: 'boolean',
+  },
+  showArrow: {
+    default: true,
+    type: 'boolean',
+  },
+  side: {
+    default: 'bottom' as any,
+    options: [
+      'top',
+      'bottom',
+      'left',
+      'right',
+    ],
+    type: 'array',
+  },
+  size: {
+    default: selectVariants.defaultVariants.size,
+    options: Object.keys(selectVariants.variants.size),
+    type: 'array',
+  },
+})
+</script>
+
+<template>
+  <SelectRoot
+    :disabled="controls.disabled"
+    :multiple="controls.multiple"
+    :size="controls.size"
+  >
+    <SelectTrigger class="max-inline-sm">
+      <SelectValue placeholder="Select a fruit" />
+    </SelectTrigger>
+
+    <SelectContent
+      :align="controls.align"
+      :position="controls.contentPosition"
+      :side="controls.side"
+      :show-arrow="controls.showArrow"
+    >
+      <SelectGroup>
+        <SelectItem value="apple">
+          Apple
+        </SelectItem>
+
+        <SelectItem value="banana">
+          Banana
+        </SelectItem>
+
+        <SelectItem value="orange">
+          Orange
+        </SelectItem>
+      </SelectGroup>
+
+      <SelectGroup>
+        <SelectLabel>
+          Berries
+        </SelectLabel>
+
+        <SelectItem
+          value="strawberry"
+          disabled
+        >
+          Strawberry
+        </SelectItem>
+
+        <SelectItem value="blueberry">
+          Blueberry
+        </SelectItem>
+
+        <SelectItem value="raspberry">
+          Raspberry
+        </SelectItem>
+      </SelectGroup>
+    </SelectContent>
+  </SelectRoot>
+</template>
