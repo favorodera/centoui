b0e1173c12e3fefd9c98fc675e1e7666435ce163
Favour Emeka <favorodera@gmail.com>
2026-06-20 19:13:58 +0100

    feat(select): apply root size to select value
    - inject root context into select-value
    - update classNames to include root size variant

--- packages/core/src/components/select/select-value.vue
+++ packages/core/src/components/select/select-value.vue
@@ -3,6 +3,7 @@ import { reactiveOmit } from '@vueuse/core'
 import { SelectValue, useForwardProps } from 'reka-ui'
 import { computed } from 'vue'
 import {
+  injectRootContext,
   type SelectValueProps,
   type SelectValueSlots,
   selectVariants,
@@ -11,11 +12,19 @@ import {
 defineSlots<SelectValueSlots>()
 
 const props = defineProps<SelectValueProps>()
+
+const rootContext = injectRootContext()
+
 const delegatedProps = reactiveOmit(props, 'class')
+
 const forwardedProps = useForwardProps(delegatedProps)
 
 const { value } = selectVariants()
-const classNames = computed(() => value({ class: props.class }))
+
+const classNames = computed(() => value({
+  class: props.class,
+  size: rootContext.size,
+}))
 </script>
 
 <template>
