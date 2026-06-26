123576e0910e85a6d1a475185b0bff6ae55efff0
Favour Emeka <favorodera@gmail.com>
2026-06-26 19:59:05 +0100

    feat(checkbox): add styles and size variants
    - apply base styles to root and indicator slots
    - define size variants for sm, md, and lg
    - add state-based styling for unchecked state

--- packages/core/src/components/checkbox/index.ts
+++ packages/core/src/components/checkbox/index.ts
@@ -6,14 +6,36 @@ export const checkboxVariants = tv({
     size: 'md',
   },
   slots: {
-    indicator: '',
-    root: '',
+    indicator: `grid place-content-center text-current transition-none`,
+    root: `
+      peer relative inline-flex justify-center shrink-0 items-center border
+      outline-none transition-colors duration-150 ease-in-out border-input
+      text-primary-foreground bg-primary
+
+      focus-visible:ring-2 focus-visible:ring-ring
+
+      aria-invalid:ring-2 aria-invalid:ring-error
+
+      disabled:pointer-events-none disabled:opacity-65
+
+      data-[state=unchecked]:bg-transparent
+      data-[state=unchecked]:text-foreground
+    `,
   },
   variants: {
     size: {
-      lg: {},
-      md: {},
-      sm: {},
+      lg: {
+        indicator: `*:data-[slot=icon]:block-4 *:data-[slot=icon]:inline-4`,
+        root: 'inline-5.5 block-5.5 rounded-md',
+      },
+      md: {
+        indicator: `*:data-[slot=icon]:block-3.5 *:data-[slot=icon]:inline-3.5`,
+        root: 'inline-5 block-5 rounded-md',
+      },
+      sm: {
+        indicator: `*:data-[slot=icon]:block-3 *:data-[slot=icon]:inline-3`,
+        root: 'inline-4 block-4 rounded-sm',
+      },
     },
   },
 })
