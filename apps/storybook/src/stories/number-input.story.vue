eeb8c172badb74fdd967a9b5686b6edcea3e20da
Favour Emeka <favorodera@gmail.com>
2026-06-30 21:28:29 +0100

    feat(storybook): add story for number input component

---
+++ apps/storybook/src/stories/number-input.story.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { NumberInput, numberInputVariants } from '#centoui/components/number-input'
+import { useStory } from '@/composables/use-story'
+
+const controls = useStory('NumberInput', {
+  disabled: {
+    default: false,
+    type: 'boolean',
+  },
+  invalid: {
+    default: false,
+    type: 'boolean',
+  },
+  size: {
+    default: numberInputVariants.defaultVariants.size,
+    options: Object.keys(numberInputVariants.variants.size),
+    type: 'array',
+  },
+})
+
+const inputModel = ref()
+</script>
+
+<template>
+  <NumberInput
+    id="number-input"
+    v-model:model-value="inputModel"
+    :size="controls.size"
+    :disabled="controls.disabled"
+    :aria-invalid="controls.invalid"
+    :max="10"
+    name="number-input"
+    placeholder="Enter a number..."
+  />
+
+  <p class="text-sm text-muted-foreground">
+    {{ inputModel }}
+  </p>
+</template>
