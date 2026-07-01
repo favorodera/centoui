97b359de600442b09f7119e14d39207b1e6c018e
Favour Emeka <favorodera@gmail.com>
2026-07-01 08:57:03 +0100

    feat(core): add FieldSet component

---
+++ packages/core/src/components/field/field-set.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { type FieldSetProps, fieldVariants } from '.'
+
+const props = defineProps<FieldSetProps>()
+
+const { fieldSet } = fieldVariants()
+
+const classNames = computed(() => fieldSet({
+  class: props.class,
+}))
+</script>
+
+<template>
+  <fieldset
+    data-slot="field-set"
+    :class="classNames"
+  >
+    <slot />
+  </fieldset>
+</template>
