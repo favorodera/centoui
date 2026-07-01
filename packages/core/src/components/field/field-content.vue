c0dbe0f127e082cf858a73f20d2f954a83b8d719
Favour Emeka <favorodera@gmail.com>
2026-07-01 10:30:46 +0100

    feat(core): add field-content component
    - Implement FieldContent using Reka-ui Primitive
    - Add support for variants and class forwarding

---
+++ packages/core/src/components/field/field-content.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
+import { Primitive, useForwardProps } from 'reka-ui'
+import { computed } from 'vue'
+import { type FieldContentProps, fieldVariants } from '.'
+
+const props =defineProps<FieldContentProps>()
+
+const delegatedProps = reactiveOmit(props, 'class')
+
+const forwardedProps = useForwardProps(delegatedProps)
+
+const { content } = fieldVariants()
+
+const classNames = computed(() => content({
+  class: props.class,
+}))
+</script>
+
+<template>
+  <Primitive
+    data-slot="field-content"
+    v-bind="forwardedProps"
+    :class="classNames"
+  >
+    <slot />
+  </Primitive>
+</template>
