2c42b99825b4a133cc0585f9994a073ada3c8f01
Favour Emeka <favorodera@gmail.com>
2026-07-01 08:56:57 +0100

    feat(field): add FieldLegend component

---
+++ packages/core/src/components/field/field-legend.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { type FieldLegendProps, fieldVariants } from '.'
+
+const props = withDefaults(defineProps<FieldLegendProps>(), {
+  variant: 'legend',
+})
+
+const { fieldLegend } = fieldVariants()
+
+const classNames = computed(() => fieldLegend({
+  class: props.class,
+  fieldLegendVariant: props.variant,
+}))
+</script>
+
+<template>
+  <legend
+    data-slot="field-legend"
+    :data-variant="variant"
+    :class="classNames"
+  >
+    <slot />
+  </legend>
+</template>
