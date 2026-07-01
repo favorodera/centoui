7aa903a6649075b43fceebcff3cb53672ebbde39
Favour Emeka <favorodera@gmail.com>
2026-07-01 09:18:03 +0100

    feat(storybook): add field component story

---
+++ apps/storybook/src/stories/field.story.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { FieldLegend, FieldSet } from '#centoui/components/field'
+import { useStory } from '@/composables/use-story'
+
+useStory('Field', {})
+</script>
+
+<template>
+  <FieldSet>
+    <FieldLegend>Address Information</FieldLegend>
+  </FieldSet>
+</template>
