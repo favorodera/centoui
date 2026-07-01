5201257957bc45bac4fcd2ea74336f5c775b2a36
Favour Emeka <favorodera@gmail.com>
2026-07-01 09:22:06 +0100

    feat(field): add FieldGroup component

---
+++ packages/core/src/components/field/field-group.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
+import { Primitive, useForwardProps } from 'reka-ui'
+import { computed } from 'vue'
+import { type FieldGroupProps, fieldVariants } from '.'
+
+const props = defineProps<FieldGroupProps>()
+
+const delegatedProps = reactiveOmit(props, 'class')
+
+const forwardedProps = useForwardProps(delegatedProps)
+
+const { group } = fieldVariants()
+
+const classNames = computed(() => group({
+  class: props.class,
+}))
+</script>
+
+<template>
+  <primitive
+    data-slot="field-group"
+    v-bind="forwardedProps"
+    :class="classNames"
+  >
+    <slot />
+  </primitive>
+</template>
