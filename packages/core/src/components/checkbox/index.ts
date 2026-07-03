f9e448bd16b0089b7e7c54b303cdf15cec2ccd76
Favour Emeka <favorodera@gmail.com>
2026-07-03 21:59:34 +0100

    style(checkbox): remove unnecessary transition-colors

--- packages/core/src/components/checkbox/index.ts
+++ packages/core/src/components/checkbox/index.ts
@@ -9,8 +9,7 @@ export const checkboxVariants = tv({
     indicator: `grid place-content-center text-current transition-none`,
     root: `
       peer relative inline-flex justify-center shrink-0 items-center border
-      outline-none transition-colors border-input text-primary-foreground
-      bg-primary
+      outline-none border-input text-primary-foreground bg-primary
 
       focus-visible:ring-2 focus-visible:ring-ring
 
