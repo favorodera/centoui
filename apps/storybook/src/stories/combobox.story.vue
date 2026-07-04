6bc78eb625c6e92ff983c4ced673c74e84f904b1
Favour Emeka <favorodera@gmail.com>
2026-07-04 20:04:36 +0100

    refactor(storybook): remove unnecessary data-slot attribute
    - Remove redundant attribute from ComboboxInput
    - Clean up storybook component configuration

--- apps/storybook/src/stories/combobox.story.vue
+++ apps/storybook/src/stories/combobox.story.vue
@@ -88,7 +88,6 @@ const options = [
     :size="controls.size"
   >
     <ComboboxInput
-      data-slot="input-group-control"
       placeholder="Search a fruit"
       :aria-invalid="controls.invalid"
     />
