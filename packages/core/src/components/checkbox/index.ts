684ec4800184658793373e76d97846c011c433dd
Favour Emeka <favorodera@gmail.com>
2026-07-06 18:39:03 +0100

    chore(deps): update @favorodera/eslint-config to 1.0.3
    - Update eslint-config to version 1.0.3
    - Add eslint-plugin-vuejs-accessibility
    - Update checkbox unchecked state styling

--- packages/core/src/components/checkbox/index.ts
+++ packages/core/src/components/checkbox/index.ts
@@ -17,7 +17,7 @@ export const checkboxVariants = tv({
 
       disabled:pointer-events-none disabled:opacity-65
 
-      data-[state=unchecked]:bg-transparent
+      data-[state=unchecked]:bg-muted
       data-[state=unchecked]:text-foreground
     `,
   },
